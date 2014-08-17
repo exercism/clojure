d81fdc52e6f29a66e2af9f41d54acc3028415eb0
Simon Jakobi <simon.jakobi@gmail.com>
2014-08-17 12:47:45 +0200

    nucleotide-count: Remove special treatment of uracil
    For the related discussion see https://github.com/exercism/xpython/pull/98.
    
    This also changes the namespace of the example solution from "dna" to
    "nucleotide_count".

--- nucleotide-count/example.clj
+++ nucleotide-count/example.clj
@@ -2,7 +2,6 @@
   (:refer-clojure :exclude [count]))
 
 (def ^{:private :const} dna-nucleotide? #{\A \C \G \T})
-(def ^{:private :const} rna-nucleotide? #{\A \C \G \U})
 
 (def ^{:private :const} base-count
   (apply hash-map (interleave dna-nucleotide? (repeat 0))))
@@ -15,7 +14,7 @@
 (defn count
   "count occurrences of nucleotide in strand"
   [nucleotide strand]
-  (cond
-    (dna-nucleotide? nucleotide) ((nucleotide-counts strand) nucleotide)
-    (rna-nucleotide? nucleotide) 0
-    :else (throw (Exception. (str "invalid nucleotide '" nucleotide "'")))))
+  (if
+    (dna-nucleotide? nucleotide)
+    ((nucleotide-counts strand) nucleotide)
+    (throw (Exception. (str "invalid nucleotide '" nucleotide "'")))))
