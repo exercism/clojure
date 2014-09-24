4fae69526892e0e112c5f1a523d2281d7a670eee
Austin Lyons <austinlyons@users.noreply.github.com>
2014-09-24 07:25:01 -0500

    Update bob_test.clj
    Woah --> Whoa

--- bob/bob_test.clj
+++ bob/bob_test.clj
@@ -5,7 +5,7 @@
   (is (= "Whatever." (bob/response-for "Tom-ay-to, tom-aaaah-to."))))
 
 (deftest responds-to-shouts
-  (is (= "Woah, chill out!" (bob/response-for "WATCH OUT!"))))
+  (is (= "Whoa, chill out!" (bob/response-for "WATCH OUT!"))))
 
 (deftest responds-to-questions
   (is (= "Sure." (bob/response-for "Does this cryogenic chamber make me look fat?"))))
@@ -17,16 +17,16 @@
   (is (= "Whatever." (bob/response-for "It's OK if you don't want to go to the DMV."))))
 
 (deftest responds-to-forceful-questions
-  (is (= "Woah, chill out!" (bob/response-for "WHAT THE HELL WERE YOU THINKING?"))))
+  (is (= "Whoa, chill out!" (bob/response-for "WHAT THE HELL WERE YOU THINKING?"))))
 
 (deftest responds-to-shouting-with-special-characters
-  (is (= "Woah, chill out!" (bob/response-for "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"))))
+  (is (= "Whoa, chill out!" (bob/response-for "ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!"))))
 
 (deftest responds-to-shouting-numbers
-  (is (= "Woah, chill out!" (bob/response-for "1, 2, 3 GO!"))))
+  (is (= "Whoa, chill out!" (bob/response-for "1, 2, 3 GO!"))))
 
 (deftest responds-to-shouting-with-no-exclamation-mark
-  (is (= "Woah, chill out!" (bob/response-for "I HATE YOU"))))
+  (is (= "Whoa, chill out!" (bob/response-for "I HATE YOU"))))
 
 (deftest responds-to-statement-containing-question-mark
   (is (= "Whatever." (bob/response-for "Ending with ? means a question."))))
