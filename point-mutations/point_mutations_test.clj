0134f9b3fc7a56e1d2f82962bf96e1a1410e68ef
Solomon White <rubysolo@gmail.com>
2014-09-27 09:40:49 -0600

    make hamming conform to official definition
    Closes #25

--- point-mutations/point_mutations_test.clj
+++ point-mutations/point_mutations_test.clj
@@ -1,33 +1,23 @@
-(ns point-mutations.test (:require [clojure.test :refer :all]))
-(load-file "point_mutations.clj")
+(ns point_mutations-test
+  (:require [clojure.test :refer :all]
+            [point_mutations :refer :all]))
+
 
 (deftest no-difference-between-empty-strands
-  (is (= 0 (point-mutations/hamming-distance "" ""))))
+  (is (= 0 (point_mutations/hamming-distance "" ""))))
 
 (deftest no-difference-between-identical-strands
-  (is (= 0 (point-mutations/hamming-distance "GGACTGA" "GGACTGA"))))
+  (is (= 0 (point_mutations/hamming-distance "GGACTGA" "GGACTGA"))))
 
 (deftest complete-hamming-distance-in-small-strand
-  (is (= 3 (point-mutations/hamming-distance "ACT" "GGA"))))
-
-(deftest hamming-distance-in-off-by-one-strand
-  (is (= 19 (point-mutations/hamming-distance "GGACGGATTCTGACCTGGACTAATTTTGGGG" "AGGACGGATTCTGACCTGGACTAATTTTGGGG"))))
+  (is (= 3 (point_mutations/hamming-distance "ACT" "GGA"))))
 
 (deftest small-hamming-distance-in-middle-somewhere
-  (is (= 1 (point-mutations/hamming-distance "GGACG" "GGTCG"))))
+  (is (= 1 (point_mutations/hamming-distance "GGACG" "GGTCG"))))
 
 (deftest larger-distance
-  (is (= 2 (point-mutations/hamming-distance "ACCAGGG" "ACTATGG"))))
+  (is (= 2 (point_mutations/hamming-distance "ACCAGGG" "ACTATGG"))))
 
-(deftest ignores-extra-length-on-other-strand-when-longer
-  (is (= 3 (point-mutations/hamming-distance "AAACTAGGGG" "AGGCTAGCGGTAGGAC"))))
-
-(deftest ignores-extra-length-on-original-strand-when-longer
-  (is (= 5 (point-mutations/hamming-distance "GACTACGGACAGGGTAGGGAAT" "GACATCGCACACC"))))
-
-(deftest does-not-actually-shorten-original-strand
-  (is (= 1 (point-mutations/hamming-distance "AGACAACAGCCAGCCGCCGGATT" "AGGCAA")))
-  (is (= 4 (point-mutations/hamming-distance "AGACAACAGCCAGCCGCCGGATT" "AGACATCTTTCAGCCGCCGGATTAGGCAA")))
-  (is (= 1 (point-mutations/hamming-distance "AGACAACAGCCAGCCGCCGGATT" "AGG"))))
-
-(run-tests)
+(deftest undefined-when-lengths-are-different
+  (is (= nil (point_mutations/hamming-distance "AAAC" "TAGGGGAGGCTAGCGGTAGGAC")))
+  (is (= nil (point_mutations/hamming-distance "GACTACGGACAGGGTAACATAG" "GACA"))))
