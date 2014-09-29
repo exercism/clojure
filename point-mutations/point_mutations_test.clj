8822cbc25c3fec4b6d371e3fef1c13de8320716d
Jim Anders <janders223@users.noreply.github.com>
2014-09-28 20:59:52 -0400

    Consistent file loading and namespace conventions

--- point-mutations/point_mutations_test.clj
+++ point-mutations/point_mutations_test.clj
@@ -1,25 +1,23 @@
-(ns point_mutations-test
-  (:require [clojure.test :refer :all]
-            [point_mutations :refer :all]))
-
+(ns point-mutations.test (:require [clojure.test :refer :all]))
+(load-file "point_mutations.clj")
 
 (deftest no-difference-between-empty-strands
-  (is (= 0 (point_mutations/hamming-distance "" ""))))
+  (is (= 0 (point-mutations/hamming-distance "" ""))))
 
 (deftest no-difference-between-identical-strands
-  (is (= 0 (point_mutations/hamming-distance "GGACTGA" "GGACTGA"))))
+  (is (= 0 (point-mutations/hamming-distance "GGACTGA" "GGACTGA"))))
 
 (deftest complete-hamming-distance-in-small-strand
-  (is (= 3 (point_mutations/hamming-distance "ACT" "GGA"))))
+  (is (= 3 (point-mutations/hamming-distance "ACT" "GGA"))))
 
 (deftest small-hamming-distance-in-middle-somewhere
-  (is (= 1 (point_mutations/hamming-distance "GGACG" "GGTCG"))))
+  (is (= 1 (point-mutations/hamming-distance "GGACG" "GGTCG"))))
 
 (deftest larger-distance
-  (is (= 2 (point_mutations/hamming-distance "ACCAGGG" "ACTATGG"))))
+  (is (= 2 (point-mutations/hamming-distance "ACCAGGG" "ACTATGG"))))
 
 (deftest undefined-when-lengths-are-different
-  (is (= nil (point_mutations/hamming-distance "AAAC" "TAGGGGAGGCTAGCGGTAGGAC")))
-  (is (= nil (point_mutations/hamming-distance "GACTACGGACAGGGTAACATAG" "GACA"))))
+  (is (= nil (point-mutations/hamming-distance "AAAC" "TAGGGGAGGCTAGCGGTAGGAC")))
+  (is (= nil (point-mutations/hamming-distance "GACTACGGACAGGGTAACATAG" "GACA"))))
 
 (run-tests)
