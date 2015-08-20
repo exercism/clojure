82efa973e625c80ba2e54d271143f849b96350a2
Eric Bailey <eric@ericb.me>
2015-08-19 19:33:01 -0500

    Add _test/check_exercises.clj
    Ironically, it uses `load-file`. :laughing:

---
+++ _test/check_exercises.clj
@@ -0,0 +1,11 @@
+(ns check-exercises
+  (:require [cheshire.core :as json]
+            [clojure.string :as string]
+            [clojure.test :refer :all]))
+
+(deftest check-exercises
+  (let [problems (-> (slurp "config.json") (json/parse-string true) :problems)]
+    (doseq [problem problems]
+      (load-file (str problem "/example.clj"))
+      (load-file (str problem "/" (string/replace problem \- \_) "_test.clj")))
+    (is (zero? (:fail (run-all-tests #"-test"))))))
