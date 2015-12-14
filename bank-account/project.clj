a3ed950edeac3c0ae43e35218155ed63946f39c7
Eric Bailey <eric@ericb.me>
2015-12-13 19:05:43 -0600

    Remove broken bank-account
    $ bin/fetch-configlet
    $ bin/configlet .
    Evaluating .
    -> [bank-account] should not be implemented.
    $ rm -r bank-account
    $ bin/configlet .
    Evaluating .
    ... OK

--- bank-account/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject bank-account "0.1.0-SNAPSHOT"
-  :description "bank-account exercise."
-  :url "https://github.com/exercism/xclojure/tree/master/bank-account"
-  :source-paths [""]
-  :test-paths [""]
-  :dependencies [[org.clojure/clojure "1.7.0"]])
