9989c3b19fc45c7cf567aabdc27205ba343888d8
Matthaus Owens <matthaus@puppetlabs.com>
2015-12-27 23:30:05 -0800

    Add project.clj for pascals-triangle

---
+++ pascals-triangle/project.clj
@@ -0,0 +1,6 @@
+(defproject pascals-triangle "0.1.0-SNAPSHOT"
+  :description "pascal's triangle exercise."
+  :url "https://github.com/exercism/xclojure/tree/master/pascals-triangle"
+  :source-paths [""]
+  :test-paths [""]
+  :dependencies [[org.clojure/clojure "1.7.0"]])
