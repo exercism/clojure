b7e8cda5565d5f45df8f809c97db4774651214fe
Matthaus Owens <matthaus@puppetlabs.com>
2015-12-28 23:25:47 -0800

    Add trinary exercise to clojure track
    This adds a trinary exercise to the clojure track. The tests were ported
    from [xruby](https://github.com/exercism/xruby/blob/master/trinary/trinary_test.rb).

---
+++ trinary/project.clj
@@ -0,0 +1,6 @@
+(defproject trinary "0.1.0-SNAPSHOT"
+  :description "trinary exercise."
+  :url "https://github.com/exercism/xclojure/tree/master/trinary"
+  :source-paths [""]
+  :test-paths [""]
+  :dependencies [[org.clojure/clojure "1.7.0"]])
