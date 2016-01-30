4ee48999d573d4439ee55e8a04f5f8b3f0b488f2
Eric Bailey <eric@ericb.me>
2016-01-30 14:02:15 -0600

    Fix #93

--- _test/check_exercises.clj
+++ _test/check_exercises.clj
@@ -7,7 +7,7 @@
 
 (deftest check-exercises
   (doseq [problem ((json/parse-string (slurp "config.json")) "problems")
-          :let [path-to-problem (partial str problem "/")
+          :let [path-to-problem (partial str "exercises/" problem "/")
                 problem-tests   (symbol (str problem "-test"))]]
     (load-file (path-to-problem "src/example.clj"))
     (load-file (path-to-problem "test/" (->snake_case problem) "_test.clj"))
