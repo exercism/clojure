4996b82b50935997b64628ba27cea5cbb6ea37f0
Eric Bailey <eric@ericb.me>
2016-02-02 13:21:35 -0600

    Add sum-of-multiples (#1)

---
+++ exercises/sum-of-multiples/project.clj
@@ -0,0 +1,4 @@
+(defproject sum-of-multiples "0.1.0-SNAPSHOT"
+  :description "sum-of-multiples exercise."
+  :url "https://github.com/exercism/xclojure/tree/master/exercises/sum-of-multiples"
+  :dependencies [[org.clojure/clojure "1.7.0"]])
