2e7651d4b903c7ec286c2deab37a813a7db5b192
Eric Bailey <eric@ericb.me>
2016-04-26 19:09:21 -0500

    Clean up robot-name-test
    Fix the scoping issues and use the optional
    second argument to `is` to provide test descriptions.
    
    Close #114
    Close #115

--- exercises/robot-name/test/robot_name_test.clj
+++ exercises/robot-name/test/robot_name_test.clj
@@ -1,28 +1,26 @@
 (ns robot-name-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest is testing]]
             robot-name))
 
-(def robbie (robot-name/robot))
+(def ^:private robbie (robot-name/robot))
+(def ^:private clutz  (robot-name/robot))
 
-(def clutz  (robot-name/robot))
+(deftest robot-name
+  (testing "robot-name"
+    (is (re-seq #"[A-Z]{2}\d{3}" (robot-name/robot-name robbie))
+        "name matches expected pattern")
+    (is (= (robot-name/robot-name robbie) (robot-name/robot-name robbie))
+        "name is persistent")
+    (is (not= (robot-name/robot-name clutz) (robot-name/robot-name robbie))
+        "different robots have different names")))
 
-(deftest name-matches-expected-pattern
-  (is (re-find (re-matcher #"[A-Z]{2}\d{3}" (robot-name/robot-name robbie)))))
-
-(deftest name-is-persistent
-  (is (= (robot-name/robot-name robbie) (robot-name/robot-name robbie))))
-
-(deftest different-robots-have-different-names
-  (is (not (= (robot-name/robot-name clutz) (robot-name/robot-name robbie)))))
-
-(def original-name (robot-name/robot-name robbie))
-(robot-name/reset-name robbie)
-
-(deftest new-name-matches-expected-pattern
-  (is (re-find (re-matcher #"[A-Z]{2}\d{3}" (robot-name/robot-name robbie)))))
-
-(deftest new-name-is-persistent
-  (is (= (robot-name/robot-name robbie) (robot-name/robot-name robbie))))
-
-(deftest new-name-is-different-than-old-name
-  (is (not (= original-name (robot-name/robot-name robbie)))))
+(deftest reset-name
+  (testing "reset-name"
+    (let [original-name (robot-name/robot-name robbie)]
+      (robot-name/reset-name robbie)
+      (is (re-seq #"[A-Z]{2}\d{3}" (robot-name/robot-name robbie))
+          "new name matches expected pattern")
+      (is (= (robot-name/robot-name robbie) (robot-name/robot-name robbie))
+          "new name is persistent")
+      (is (not= original-name (robot-name/robot-name robbie))
+          "new name is different from old name"))))
