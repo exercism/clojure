e464056cc14088066f6529236792adf18dd7c2b7
Norbert Melzer <timmelzer@gmail.com>
2016-08-14 17:39:01 +0200

    [sum-of-multiples] made function “normal”
    Before it was a multi-clause function with a single clause.

--- exercises/sum-of-multiples/src/example.clj
+++ exercises/sum-of-multiples/src/example.clj
@@ -4,7 +4,6 @@
   ([n]   (fn [d] (divides? n d)))
   ([n d] (zero? (rem n d))))
 
-(defn sum-of-multiples
-  ([multiples n]
-   (reduce (fn [sum x] (cond-> sum (some (divides? x) multiples) (+ x)))
-           0 (range 1 n))))
+(defn sum-of-multiples [multiples n]
+  (reduce (fn [sum x] (cond-> sum (some (divides? x) multiples) (+ x)))
+	  0 (range 1 n)))
