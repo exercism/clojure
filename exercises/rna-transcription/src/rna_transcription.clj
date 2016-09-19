87efbb06e0011425e589e3c84d40f6d718c69364
Tejas Bubane <tejasbubane@gmail.com>
2016-09-20 00:22:35 +0530

    Add some more stubs files for exercises
    Hello world was already added in #122, add to next 4 exercises
    (to make it 5)
    
    Fixes #121

---
+++ exercises/rna-transcription/src/rna_transcription.clj
@@ -0,0 +1,5 @@
+(ns rna-transcription)
+
+(defn to-rna [] ;; <- arglist goes here
+  ;; your code goes here
+)
---
+++ exercises/word-count/src/word_count.clj
@@ -0,0 +1,5 @@
+(ns word-count)
+
+(defn word-count [] ;; <- arglist goes here
+  ;; your code goes here
+)
