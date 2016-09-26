acf8746112eec53c03fd6f34107b23cdbb5fb6e7
Matthaus Owens <matthaus@puppet.com>
2016-09-25 21:19:50 -0700

    Add all-your-base exercise
    This commit adds the all-your-base exercise, with the tests ported from
    the ruby version of the exercise.

---
+++ exercises/all-your-base/project.clj
@@ -0,0 +1,4 @@
+(defproject all-your-base "0.1.0-SNAPSHOT"
+  :description "all-your-base exercise."
+  :url "https://github.com/exercism/xclojure/tree/master/exercises/all-your-base"
+  :dependencies [[org.clojure/clojure "1.8.0"]])
