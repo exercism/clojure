bc83cecd0d5dca98cfc162eab42515f621976ddb
Matthaus Owens <matthaus@puppet.com>
2016-09-25 22:28:28 -0700

    Respond to PR feedback
    This commit renames the src file to example.clj, moves all function
    arguments to be on the same line and fixes the indentation in the ->>
    form in convert.

--- exercises/all-your-base/src/example.clj
+++ exercises/all-your-base/src/example.clj
@@ -2,8 +2,7 @@
 
 (defn- digits->decimal
   "Converts a sequence of digits given in input-base into decimal format."
-  [input-base
-   digits]
+  [input-base digits]
   (loop [sum 0
          [num & nums] digits]
     (if num
@@ -12,8 +11,7 @@
 
 (defn- decimal->digits
   "Converts a decimal number into a sequence of digits in the desired output base."
-  [output-base
-   number]
+  [output-base number]
   (loop [digits nil
          num number]
     (if (zero? num)
@@ -22,14 +20,12 @@
 
 (defn convert
   "Converts a sequence of digits given in input-base into a sequence of digits in the desired output-base."
-  [input-base
-   digits
-   output-base]
+  [input-base digits output-base]
   (cond
     (some #(< % 2) (list input-base output-base)) nil
     (not-every? #(and (not (neg? %)) (< % input-base)) digits) nil
     (empty? digits) ()
     (every? #(zero? %) digits) '(0)
     :else (->> digits
-         (digits->decimal input-base)
-         (decimal->digits output-base))))
+               (digits->decimal input-base)
+               (decimal->digits output-base))))
