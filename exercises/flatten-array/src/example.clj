93ff6081f8944ce1efdc916ae72077c84116b146
Matthaus Owens <matthaus@puppet.com>
2016-10-02 11:32:12 -0700

    Update flatten example to use recur and remove

--- exercises/flatten-array/src/example.clj
+++ exercises/flatten-array/src/example.clj
@@ -4,8 +4,13 @@
 (defn flatten
   "Flattens the given list and removes any nil values"
   [coll]
-  (cond
-    (empty? coll) (list)
-    (nil? (first coll)) (flatten (rest coll))
-    (coll? (first coll)) (concat (flatten (first coll)) (flatten (rest coll)))
-    :else (cons (first coll) (flatten (rest coll)))))
+  (loop
+   [results nil
+    [elem & remaining] coll]
+    (if (or elem remaining)
+      (if (coll? elem)
+        (if (empty? elem)
+          (recur results remaining)
+          (recur results (conj remaining (rest elem) (first elem))))
+        (recur (conj results elem) remaining))
+      (reverse (remove nil? results)))))
