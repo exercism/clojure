505ac20375c524e52f89785aa78a2f2a65829bed
Matthaus Owens <matthaus@puppet.com>
2016-10-08 10:16:39 -0700

    Re-add bank account exercise
    This commit re-adds the previous bank-account exercise with an updated
    test suite. The previous test suite had a call to (shutdown-agents)
    outside of a function call, which caused it to get called before all
    tests had executed. This version of the test suite uses a fixture to
    ensure that the (shutdown-agents) call happens *after* all tests have
    executed.

---
+++ exercises/bank-account/project.clj
@@ -0,0 +1,4 @@
+(defproject bank-account "0.1.0-SNAPSHOT"
+  :description "bank-account exercise."
+  :url "https://github.com/exercism/xclojure/tree/master/exercises/bank-account"
+  :dependencies [[org.clojure/clojure "1.8.0"]])
