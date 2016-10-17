a4ad3abf617ffae36085d310bb98f695f94fad1e
object88 <object88@gmail.com>
2016-10-16 21:54:11 -0600

    Created project.clj

---
+++ exercises/flatten-array/project.clj
@@ -0,0 +1,4 @@
+(defproject flatten-array "0.1.0-SNAPSHOT"
+  :description "flatten-array exercise."
+  :url "https://github.com/exercism/xclojure/tree/master/exercises/flatten-array"
+  :dependencies [[org.clojure/clojure "1.8.0"]])
