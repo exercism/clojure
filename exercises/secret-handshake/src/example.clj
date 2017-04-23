dca22909767ada66c3a23f8d85dcaf249e3796a5
Steven Blowers <shdblowers@gmail.com>
2017-04-23 10:52:33 +0100

    updating example solution with suggestions from @haus

--- exercises/secret-handshake/src/example.clj
+++ exercises/secret-handshake/src/example.clj
@@ -1,17 +1,19 @@
 (ns secret-handshake)
 
-(defn- to-binary-seq [integer]
-  (reverse (seq (Integer/toString integer 2))))
+(defn- int->reversed-binary [int]
+  (-> int
+      (Integer/toBinaryString)
+      (reverse)))
 
 (defn- convert [integer]
   (filter some?
           (map (fn [command binary]
                  (if (= \1 binary) command))
                ["wink" "double blink" "close your eyes" "jump"]
-               (to-binary-seq integer))))
+               (int->reversed-binary integer))))
 
 (defn commands [integer]
   (let [cmds (convert integer)]
-    (if (= \1 (nth (to-binary-seq integer) 4 \0))
+    (if (= \1 (nth (int->reversed-binary integer) 4 \0))
       (reverse cmds)
       cmds)))
