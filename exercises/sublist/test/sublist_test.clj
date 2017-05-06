ab11b68aa0903b8431fa263f6ce7d55efbaeb610
Matthaus Owens <matthaus@puppet.com>
2017-05-06 10:53:15 -0700

    Add sublist test template
    This commit adds a mustache template for the sublist tests and replaces
    the tests with a generated set of tests.

--- exercises/sublist/test/sublist_test.clj
+++ exercises/sublist/test/sublist_test.clj
@@ -1,52 +1,40 @@
 (ns sublist-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest is testing]]
             sublist))
 
-(deftest empty-lists
-  (is (= (sublist/classify [] []) :equal)))
-
-(deftest empty-list-within-non-empty-list
-  (is (= (sublist/classify [] [1 2 3]) :sublist)))
-
-(deftest non-empty-list-contains-empty-list
-  (is (= (sublist/classify [1 2 3] []) :superlist)))
-
-(deftest list-equals-itself
-  (is (= (sublist/classify [1 2 3] [1 2 3]) :equal)))
-
-(deftest different-lists
-  (is (= (sublist/classify [1 2 3] [2 3 4]) :unequal)))
-
-(deftest false-start
-  (is (= (sublist/classify [1 2 5] [0 1 2 3 1 2 5 6]) :sublist)))
-
-(deftest consecutive
-  (is (= (sublist/classify [1 1 2] [0 1 1 1 2 1 2]) :sublist)))
-
-(deftest sublist-at-start
-  (is (= (sublist/classify [0 1 2] [0 1 2 3 4 5]) :sublist)))
-
-(deftest sublist-in-middle
-  (is (= (sublist/classify [2 3 4] [0 1 2 3 4 5]) :sublist)))
-
-(deftest sublist-at-end
-  (is (= (sublist/classify [3 4 5] [0 1 2 3 4 5]) :sublist)))
-
-(deftest at-start-of-superlist
-  (is (= (sublist/classify [0 1 2 3 4 5] [0 1 2]) :superlist)))
-
-(deftest in-middle-of-superlist
-  (is (= (sublist/classify [0 1 2 3 4 5] []) :superlist)))
-
-(deftest at-end-of-superlist
-  (is (= (sublist/classify [0 1 2 3 4 5] [3 4 5]) :superlist)))
-
-(deftest first-list-missing-element-from-second-list
-  (is (= (sublist/classify [1 3] [1 2 3]) :unequal)))
-
-(deftest second-list-missing-element-from-first-list
-  (is (= (sublist/classify [1 2 3] [1 3]) :unequal)))
-
-(deftest order-matters-to-a-list
-  (is (= (sublist/classify [1 2 3] [3 2 1]) :unequal)))
-
+(deftest sublist-tests
+  (testing "empty lists"
+    (is (= :equal (sublist/classify [] []))))
+  (testing "empty list within non empty list"
+    (is (= :sublist (sublist/classify [] [1 2 3]))))
+  (testing "non empty list contains empty list"
+    (is (= :superlist (sublist/classify [1 2 3] []))))
+  (testing "list equals itself"
+    (is (= :equal (sublist/classify [1 2 3] [1 2 3]))))
+  (testing "different lists"
+    (is (= :unequal (sublist/classify [1 2 3] [2 3 4]))))
+  (testing "false start"
+    (is (= :sublist (sublist/classify [1 2 5] [0 1 2 3 1 2 5 6]))))
+  (testing "consecutive"
+    (is (= :sublist (sublist/classify [1 1 2] [0 1 1 1 2 1 2]))))
+  (testing "sublist at start"
+    (is (= :sublist (sublist/classify [0 1 2] [0 1 2 3 4 5]))))
+  (testing "sublist in middle"
+    (is (= :sublist (sublist/classify [2 3 4] [0 1 2 3 4 5]))))
+  (testing "sublist at end"
+    (is (= :sublist (sublist/classify [3 4 5] [0 1 2 3 4 5]))))
+  (testing "at start of superlist"
+    (is (= :superlist (sublist/classify [0 1 2 3 4 5] [0 1 2]))))
+  (testing "in middle of superlist"
+    (is (= :superlist (sublist/classify [0 1 2 3 4 5] [2 3]))))
+  (testing "at end of superlist"
+    (is (= :superlist (sublist/classify [0 1 2 3 4 5] [3 4 5]))))
+  (testing "first list missing element from second list"
+    (is (= :unequal (sublist/classify [1 3] [1 2 3]))))
+  (testing "second list missing element from first list"
+    (is (= :unequal (sublist/classify [1 2 3] [1 3]))))
+  (testing "order matters to a list"
+    (is (= :unequal (sublist/classify [1 2 3] [3 2 1]))))
+  (testing "same digits but different numbers"
+    (is (= :unequal (sublist/classify [1 0 1] [10 1]))))
+)
