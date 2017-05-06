3517896151721179e2c4bf2ef77acab5cc85150a
Matthaus Owens <matthaus@puppet.com>
2017-05-06 10:51:57 -0700

    Refactor sublist example
    This replaces the previous nested `if`s and `when`s with a `cond` that
    calls out to a helper function in 2 cases.

--- exercises/sublist/src/example.clj
+++ exercises/sublist/src/example.clj
@@ -1,13 +1,19 @@
 (ns sublist)
 
-(defn classify [coll1 coll2]
-  (if (= coll1 coll2)
-    :equal
-    (let [len1 (count coll1)
-          len2 (count coll2)
-          [longer shorter len val] (if (> len1 len2)
-                                     [coll1 coll2 len2 :superlist]
-                                     [coll2 coll1 len1 :sublist])]
-      (or (some #(when (= % shorter) val)
-                (partition len 1 longer))
-          :unequal))))
+(defn- list-contains?
+  "Returns truthy when list2 is contained within list1, nil otherwise"
+  [list1 list2]
+  (some #(when (= % list2) val)
+        (partition (count list2) 1 list1)))
+
+(defn classify
+  "Classifies two lists based on whether coll1 is the same list, a superlist,
+  a sublist, or disjoint (unequal) from coll2."
+  [coll1 coll2]
+  (let [len1 (count coll1)
+        len2 (count coll2)]
+    (cond
+      (= coll1 coll2) :equal
+      (and (> len1 len2) (list-contains? coll1 coll2)) :superlist
+      (and (> len2 len1) (list-contains? coll2 coll1)) :sublist
+      :else :unequal)))
