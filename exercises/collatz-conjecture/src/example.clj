6bc6f7232662d938f693bd2c0525a610b665241d
Average-user <lucaspolymeris@gmail.com>
2017-09-19 20:25:21 -0300

    collatz conjecture algorithm changed

--- exercises/collatz-conjecture/src/example.clj
+++ exercises/collatz-conjecture/src/example.clj
@@ -1,10 +1,13 @@
 (ns collatz-conjecture)
 
+(defn collatz-helper [n]
+  (cond (= 1 n)   1
+        (even? n) (/ n 2)
+        :else     (inc (* 3 n))))
+
 (defn collatz [n]
   (if (> n 0)
-    (loop [t 0 n n]
-      (cond (= 1 n)   t
-            (even? n) (recur (inc t) (/ n 2))
-            :else     (recur (inc t) (inc (* 3 n)))))
+    (count (take-while #(not= 1 %)
+                       (iterate collatz-helper n)))
     (throw (IllegalArgumentException.
              "Just defined for numbers greater than 0."))))
