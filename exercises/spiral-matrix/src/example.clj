0f8d30913fe81585d4812db3ce58f66dfd9b8fac
Average-user <lucaspolymeris@gmail.com>
2017-10-09 04:42:15 -0300

    implementing thread macro

--- exercises/spiral-matrix/src/example.clj
+++ exercises/spiral-matrix/src/example.clj
@@ -3,8 +3,9 @@
 (defn spiral-matrix [m n & [s]]
   (let [row (list (map #(+ s %) (range m)))]
     (if (= 1 n) row
-      (concat row (map reverse
-                       (apply map list
-                              (spiral-matrix (dec n) m (+ s m))))))))
+      (->> (spiral-matrix (dec n) m (+ s m))
+           (apply map list)
+           (map reverse)
+           (concat row)))))
 
 (defn spiral [n] (if (zero? n) '() (spiral-matrix n n 1)))
