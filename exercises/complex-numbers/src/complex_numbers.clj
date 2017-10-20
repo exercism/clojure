413d646bb399e68a6faa9fefece9035e2a98e812
Average-user <lucaspolymeris@gmail.com>
2017-10-19 21:16:08 -0300

    Implementing complex-numbers

---
+++ exercises/complex-numbers/src/complex_numbers.clj
@@ -0,0 +1,17 @@
+(ns complex-numbers)
+
+(defn real [[a b]])
+
+(defn imaginary [[a b]])
+
+(defn abs [[a b]])
+
+(defn conjugate [[a b]])
+
+(defn add [[a b] [c d]])
+
+(defn sub [[a b] [c d]])
+
+(defn mul [[a b] [c d]])
+
+(defn div [[a b] [c d]])
