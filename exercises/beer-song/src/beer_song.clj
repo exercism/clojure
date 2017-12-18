7fa313cd6996d57e682a442aac54ccb3946b6499
Arne Schlüter <aesthaddicts@gmail.com>
2017-12-18 13:34:20 +0100

    Add beer_song.clj stub

---
+++ exercises/beer-song/src/beer_song.clj
@@ -0,0 +1,9 @@
+(ns beer-song)
+ 
+;; implement two functions: one gives you a single verse, one gives you the entire song or a part of it
+ 
+(defn verse [n])
+
+(defn sing
+  ([from])
+  ([from to])
