c69672d8a85899b9687feacb71e54f5ec226cbe0
CrowsVeldt <zechy.mahler@gmail.com>
2018-06-08 15:22:24 +0300

    Add stub for space-age

---
+++ exercises/space-age/src/space_age.clj
@@ -0,0 +1,33 @@
+(ns space-age)
+
+(defn on-mercury [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
+
+(defn on-venus [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
+
+(defn on-earth [] ;; <- Arglist goes here
+      ;; your code goes here
+      )
+
+(defn on-mars [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
+
+(defn on-jupiter [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
+
+(defn on-saturn [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
+
+(defn on-neptune [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
+
+(defn on-uranus [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
