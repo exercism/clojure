9370bf6aa649fe5ca9665aae681800cc27ba41c6
CrowsVeldt <zechy.mahler@gmail.com>
2018-06-08 15:06:34 +0300

    Add stub for robot-simulator

---
+++ exercises/robot-simulator/src/robot_simulator.clj
@@ -0,0 +1,17 @@
+(ns robot-simulator)
+
+(defn robot [] ;; <- Arglist goes here
+      ;; your code goes here
+      )
+
+(defn simulate [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
+
+(defn turn-right [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
+
+(defn turn-left [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
