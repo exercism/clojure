dac7428ddf7f6fd1756d34e10cc0821dd45116a3
CrowsVeldt <zechy.mahler@gmail.com>
2018-06-08 11:30:22 +0300

    Add protein-translation stub

---
+++ exercises/protein-translation/src/protein_translation.clj
@@ -0,0 +1,9 @@
+(ns protein-translation)
+
+(defn translate-codon [] ;; <- Arglist goes here
+      ;; your code goes here
+      )
+
+(defn translate-rna [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
