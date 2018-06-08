68b72d7aedb18b52178b8f6f094ce1c38c54f1be
CrowsVeldt <zechy.mahler@gmail.com>
2018-06-08 10:47:26 +0300

    Add phone-number stub

---
+++ exercises/phone-number/src/phone_number.clj
@@ -0,0 +1,13 @@
+(ns phone-number)
+
+(defn number [] ;; <- Arglist goes here
+      ;; your code goes here
+      )
+
+(defn area-code [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
+
+(defn pretty-print [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
