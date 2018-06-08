eb6a0de31b4db4e02a5b3a6f9f12164749111ea5
CrowsVeldt <zechy.mahler@gmail.com>
2018-06-08 15:02:49 +0300

    Add stub for robot-name

---
+++ exercises/robot-name/src/robot_name.clj
@@ -0,0 +1,13 @@
+(ns robot-name)
+
+(defn robot [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
+
+(defn robot-name [] ;; <- Arglist goes here
+      ;; your code goes here
+      )
+
+(defn reset-name [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
