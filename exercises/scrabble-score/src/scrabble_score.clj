b0aa6e6d0c9091efc5a62ca36f99d78c436754e1
CrowsVeldt <zechy.mahler@gmail.com>
2018-06-08 15:13:56 +0300

    Add stub for scrabble score

---
+++ exercises/scrabble-score/src/scrabble_score.clj
@@ -0,0 +1,9 @@
+(ns scrabble-score)
+
+(defn score-letter [] ;; <- Arglist goes here
+      ;; your code goes here
+      )
+
+(defn score-word [] ;; <- Arglist goes here
+  ;; your code goes here
+  )
