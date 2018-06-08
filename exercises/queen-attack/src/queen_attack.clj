bb2607ead68700427c30ed6472841a76eb5fd46b
CrowsVeldt <zechy.mahler@gmail.com>
2018-06-08 11:33:51 +0300

    Add queen-attack stub

---
+++ exercises/queen-attack/src/queen_attack.clj
@@ -0,0 +1,9 @@
+(ns queen-attack)
+
+(defn board-string [] ;; <- Arglist goes here
+      ;; your code goes here
+      )
+
+(defn can-attack[] ;; <- Arglist goes here
+  ;; your code goes here
+  )
