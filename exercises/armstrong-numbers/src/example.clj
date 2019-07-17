43193c815786bf65206f1910b258703547aa8662
Matt Kipps <matt.kipps@gmail.com>
2019-07-17 08:23:10 -0700

    fix armstrong reference impl

--- exercises/armstrong-numbers/src/example.clj
+++ exercises/armstrong-numbers/src/example.clj
@@ -1,6 +1,9 @@
 (ns armstrong-numbers)
 
+(defn expt [base pow]
+  (reduce * 1 (repeat pow base)))
+
 (defn armstrong? [n]
   (let [digits (map (comp read-string str) (str n))
         l      (count digits)]
-    (== n (reduce + (map #(Math/pow % l) digits)))))
+    (= n (reduce + (map #(expt % l) digits)))))
