f2901304b6f4fce91e72cd6390488de5676b9bc6
Ovidiu Miu <ovidiu.m94@gmail.com>
2019-11-25 16:58:18 +0200

    update tests for 'Triangle' exercise

--- exercises/triangle/src/triangle.clj
+++ exercises/triangle/src/triangle.clj
@@ -1,5 +1,17 @@
 (ns triangle)
 
-(defn type [] ;; <- arglist goes here
-      ;; your code goes here
+(defn is-valid? [] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn equilateral? [] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn isosceles? [] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn scalene? [] ;; <- arglist goes here
+  ;; your code goes here
 )
