5d74616040bf87f358d3c5ed69dfedfe7490f89a
Erik Schierboom <erik_schierboom@hotmail.com>
2021-06-28 13:12:33 +0200

    Fix crypto-square exercise
    The current implementation did not follow the spec. In this commit, the tests are updated as well as the example implementation.

--- exercises/practice/crypto-square/.meta/src/example.clj
+++ exercises/practice/crypto-square/.meta/src/example.clj
@@ -1,37 +1,23 @@
-(ns crypto-square)
+(ns crypto-square, (:require [clojure.string :as str]))
 
-(defn normalize-plaintext [input]
-  (.toLowerCase (clojure.string/replace input #"[^0-9a-zA-Z]" "")))
+(defn normalize-plaintext [plaintext]
+  (str/lower-case (str/replace plaintext #"[^\w]" "")))
 
-(defn square-size [input]
-  (let [s (Math/sqrt (count input))
-        si (Math/round (Math/floor s))]
-    (if (== 0 (rem s si))
-      si (inc si))))
+(defn square-size [plaintext]
+  (int (Math/ceil (Math/sqrt (count (normalize-plaintext plaintext))))))
 
-(defn plaintext-segments [input]
-  (let [text (normalize-plaintext input)
-        size (square-size text)]
-    (vec (map #(apply str %1)
-              (partition size size nil text)))))
+(defn plaintext-segments [plaintext]
+  (let [normalized-plaintext (normalize-plaintext plaintext)
+        size (square-size normalized-plaintext)]
+    (map #(apply str %) (partition size size nil normalized-plaintext))))
 
-(defn ciphertext [input]
-  (let [segments (plaintext-segments input)
-        size (count (first segments))]
-    (apply str (for [n (range size)
-                     s segments]
-                 (nth s n nil)))))
+(defn- padded-segments [plaintext pad]
+  (let [segments (plaintext-segments plaintext)
+        r (count (first segments))]
+        (map #(concat % (take (- r (count %)) (repeat pad))) segments)))
 
-(defn normalize-ciphertext [input]
-  (let [cipher                  (ciphertext input)
-        cipher-length           (count cipher)
-        square-max              (square-size cipher)
-        square-longest          (int (Math/ceil (/ (count cipher) square-max)))
-        square-shortest         (int (Math/floor (/ (count cipher) square-max)))
-        square-count-long-sides (- cipher-length (* square-max square-shortest))]
-    (apply str (interpose " "
-                          (map #(apply str %1)
-                               (concat (partition square-longest square-longest nil
-                                                  (take (* square-count-long-sides square-longest) cipher))
-                                       (partition square-shortest square-shortest nil
-                                                  (take-last (- cipher-length (* square-count-long-sides square-longest)) cipher))))))))
+(defn ciphertext [plaintext]
+  (apply str (apply mapcat vector (padded-segments plaintext nil))))
+
+(defn normalize-ciphertext [plaintext]
+  (str/join " " (map #(apply str %) (apply map vector (padded-segments plaintext \ )))))
