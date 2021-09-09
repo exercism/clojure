e314036c0c7821bb13beb0eea3b22be0bf9fe873
Bobby Towers <porkostomus@mail.com>
2021-09-09 05:36:54 -0700

    [v3] Add sequential destructuring exercise (#337)
    * add sequential destructuring exercise
    
    * add hints
    
    * add config.json
    
    * remove redundant files
    
    * add markdown heading
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    
    * cite exercise name in "forked_from"
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    
    * use proper namespace
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    
    * fix ns in source file
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    
    * fix test ns
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    
    * fix require ns
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    
    * document concept
    
    * add concept to config.json
    
    * add Leiningen config
    
    * edit blurb
    
    * add markdown heading
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>
    
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

---
+++ exercises/concept/elyses-destructured-enchantments/project.clj
@@ -0,0 +1,4 @@
+(defproject elyses-destructured-enchantments "0.1.0-SNAPSHOT"
+  :description "elyses-destructured-enchantments exercise."
+  :url "https://github.com/exercism/clojure/tree/main/exercises/concept/elyses-destructured-enchantments"
+  :dependencies [[org.clojure/clojure "1.10.0"]])
---
+++ exercises/concept/elyses-destructured-enchantments/src/enchantments.clj
@@ -0,0 +1,33 @@
+(ns elyses-destructured-enchantments)
+
+(defn first-card
+  "Returns the first card from deck."
+  [deck]
+)
+
+(defn second-card
+  "Returns the second card from deck."
+  [deck]
+)
+
+(defn swap-top-two-cards
+  "Returns the deck with first two items reversed."
+  [deck]
+)
+
+(defn discard-top-card
+  "Returns a vector containing the first card and
+   a vector of the remaining cards in the deck."
+  [deck]
+)
+
+(def face-cards
+  ["jack" "queen" "king"])
+
+(defn insert-face-cards
+  "Returns the deck with face cards between its head and tail."
+  [deck]
+)
+
+(comment
+  )
