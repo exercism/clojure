5c2dbabb0affd53e09597b097dd3e963898b48c8
porkostomus <porkostomus@mail.com>
2021-09-18 20:41:16 -0700

    test all exercises using test-runner

--- test.clj
+++ test.clj
@@ -3,28 +3,74 @@
 (require
  '[cheshire.core :as json]
  '[clojure.string :as str]
- '[clojure.test :refer [deftest is run-tests successful? use-fixtures]])
+ '[babashka.classpath :as cp]
+ '[clojure.java.shell :as shell])
 
 (defn- ->snake_case [s] (str/replace s \- \_))
 
-(deftest check-practice-exercises
-  (doseq [exercise (((json/parse-string (slurp "config.json")) "exercises") "practice")
-          :let [slug             (exercise "slug")
-                path-to-exercise (partial str "exercises/practice/" slug "/")
-                exercise-tests   (symbol (str slug "-test"))]]
-    (load-file (path-to-exercise ".meta/src/example.clj"))
-    (load-file (path-to-exercise "test/" (->snake_case slug) "_test.clj"))
-(is (successful? (run-tests exercise-tests)))))
+(def root-dir (first *command-line-args*))
+(def test-runner-dir (second *command-line-args*))
 
-(deftest check-concept-exercises
-  (doseq [exercise (((json/parse-string (slurp "config.json")) "exercises") "concept")
-          :let [slug             (exercise "slug")
-                path-to-exercise (partial str "exercises/concept/" slug "/")
-                exercise-tests   (symbol (str slug "-test"))]]
-    (load-file (path-to-exercise ".meta/exemplar.clj"))
-    (load-file (path-to-exercise "test/" (->snake_case slug) "_test.clj"))
-    (is (successful? (run-tests exercise-tests)))))
+(cp/add-classpath root-dir)
 
-(let [report (run-tests)]
-  (System/exit (+ (:fail report)
-                  (:error report))))
+(def practice-root (str root-dir "exercises/practice/"))
+(def concept-root (str root-dir "exercises/concept/"))
+
+(defn copy-practice-example! [slug]
+  (shell/sh "cp"
+            (str practice-root slug "/.meta/src/example.clj")
+            (str practice-root slug "/.meta/src/" (->snake_case slug) ".clj")))
+
+(defn replace-practice-example! [slug]
+  (shell/sh "mv"
+            (str practice-root slug "/.meta/src/" (->snake_case slug) ".clj")
+            (str practice-root slug "/src/" (->snake_case slug) ".clj")))
+
+(defn practice-pass? [slug]
+  (let [copy (copy-practice-example! slug)
+        replace (replace-practice-example! slug)]
+    (= "pass" ((json/parse-string
+                (:out (shell/sh (str test-runner-dir "test-runner.clj")
+                                slug
+                                (str practice-root slug "/")
+                                (str practice-root slug "/"))))
+               "status"))))
+
+(defn copy-concept-example! [slug]
+  (shell/sh "cp"
+            (str concept-root slug "/.meta/exemplar.clj")
+            (str concept-root slug "/.meta/" (->snake_case slug) ".clj")))
+
+(defn replace-concept-example! [slug]
+  (shell/sh "mv"
+            (str concept-root slug "/.meta/" (->snake_case slug) ".clj")
+            (str concept-root slug "/src/" (->snake_case slug) ".clj")))
+
+(defn concept-pass? [slug]
+  (let [copy (copy-concept-example! slug)
+        replace (replace-concept-example! slug)]
+    (= "pass" ((json/parse-string
+                (:out (shell/sh (str test-runner-dir "test-runner.clj")
+                                slug
+                                (str concept-root slug "/")
+                                (str concept-root slug "/"))))
+               "status"))))
+
+(def practice-exercises
+  (map #(% "slug") (((json/parse-string (slurp (str root-dir "config.json"))) "exercises") "practice")))
+
+(def concept-exercises
+  (map #(% "slug") (((json/parse-string (slurp (str root-dir "config.json"))) "exercises") "concept")))
+
+(defn check-practice-exercises! []
+  (for [exercise practice-exercises]
+    {(keyword exercise) (practice-pass? exercise)}))
+
+(defn check-concept-exercises! []
+  (for [exercise concept-exercises]
+    {(keyword exercise) (concept-pass? exercise)}))
+
+(let [results (into (check-concept-exercises!)
+                     (check-practice-exercises!))]
+  {:tested (count results)
+   :fails (filter false? results)})
