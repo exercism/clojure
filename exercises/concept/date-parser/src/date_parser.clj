62a78a3464cd4a5ff99f308ddefe3c3fa86054d5
porkostomus <porkostomus@mail.com>
2021-09-19 18:59:33 -0700

    create exemplar/stub files

--- exercises/concept/date-parser/src/date_parser.clj
+++ exercises/concept/date-parser/src/date_parser.clj
@@ -1,58 +1,39 @@
 (ns date-parser)
 
-(def day "\\d{1,2}")
-(def month "\\d{1,2}")
-(def year "\\d{4}")
+(def day)
+(def month)
+(def year)
 
-(def days "Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday")
+(def days)
 
 (defn day-names [s]
-  (re-matches (re-pattern days) s))
+  )
 
-(def months "January|February|March|April|May|June|July|August|September|October|November|December")
+(def months)
 
 (defn month-names [s]
-  (re-matches (re-pattern months) s))
+  )
 
 (defn capture-month [s]
-  (let [matcher (re-matcher (re-pattern (str "(?<month>" month ")")) s)]
-    (when (.matches matcher)
-      {:month (.group matcher "month")})))
+  )
 
 (defn capture-day [s]
-  (let [matcher (re-matcher (re-pattern (str "(?<day>" day ")")) s)]
-    (when (.matches matcher)
-      {:day (.group matcher "day")})))
+  )
 
 (defn capture-year [s]
-  (let [matcher (re-matcher (re-pattern (str "(?<year>" year ")")) s)]
-    (when (.matches matcher)
-      {:year (.group matcher "year")})))
+  )
 
 (defn capture-month-name [s]
-  {:month-name   (first (re-find (re-pattern (str "(?<month>" months ")")) s))})
+  )
 
 (defn capture-day-name [s]
-  {:day-name (first (re-find (re-pattern (str "(?<day>" days ")")) s))})
+  )
 
 (defn capture-numeric-date [s]
-  (let [matcher (re-matcher (re-pattern (str "(?<day>" day ")/(?<month>" month ")/(?<year>" year ")")) s)]
-    (when (.matches matcher)
-      {:day (.group matcher "day")
-       :month (.group matcher "month")
-       :year (.group matcher "year")})))
+  )
 
 (defn capture-month-name-date [s]
-  (let [matcher (re-matcher (re-pattern (str "(?<month>" months ") (?<day>" day "), (?<year>" year ")")) s)]
-    (when (.matches matcher)
-      {:month-name (.group matcher "month")
-       :day (.group matcher "day")
-       :year (.group matcher "year")})))
+  )
 
 (defn capture-day-month-name-date [s]
-  (let [matcher (re-matcher (re-pattern (str "(?<dayname>" days "), (?<month>" months ") (?<day>" day "), (?<year>" year ")")) s)]
-    (when (.matches matcher)
-      {:day-name (.group matcher "dayname")
-       :month-name (.group matcher "month")
-       :day (.group matcher "day")
-       :year (.group matcher "year")})))
+  )
