7f1ba2fba5a77b6d6b9884f9fa2e9f40ba134229
porkostomus <porkostomus@mail.com>
2021-09-20 20:29:42 -0700

    add task metadata

--- exercises/concept/tracks-on-tracks-on-tracks/test/tracks_on_tracks_on_tracks_test.clj
+++ exercises/concept/tracks-on-tracks-on-tracks/test/tracks_on_tracks_on_tracks_test.clj
@@ -2,25 +2,25 @@
   (:require [clojure.test :refer [deftest is]]
             tracks-on-tracks-on-tracks))
 
-(deftest list-empty-test
+(deftest ^:task1 list-empty-test
   (is (= '() (tracks-on-tracks-on-tracks/new-list))))
 
-(deftest list-add-test
+(deftest ^:task2 list-add-test
   (is (= '("JavaScript" "Java" "Lisp" "Clojure")
          (-> (tracks-on-tracks-on-tracks/new-list)
-              (tracks-on-tracks-on-tracks/add-language "Clojure")
-              (tracks-on-tracks-on-tracks/add-language "Lisp")
-              (tracks-on-tracks-on-tracks/add-language "Java")
-              (tracks-on-tracks-on-tracks/add-language "JavaScript")))))
+             (tracks-on-tracks-on-tracks/add-language "Clojure")
+             (tracks-on-tracks-on-tracks/add-language "Lisp")
+             (tracks-on-tracks-on-tracks/add-language "Java")
+             (tracks-on-tracks-on-tracks/add-language "JavaScript")))))
 
-(deftest first-test
+(deftest ^:task3 first-test
   (is (= "Lisp" (tracks-on-tracks-on-tracks/first-language '("Lisp" "Clojure")))))
 
-(deftest list-remove-test
+(deftest ^:task4 list-remove-test
   (is (= '("Clojure") (tracks-on-tracks-on-tracks/remove-language '("Lisp" "Clojure")))))
 
-(deftest list-count-test
+(deftest ^:task5 list-count-test
   (is (= 3 (tracks-on-tracks-on-tracks/count-languages '("JavaScript" "Java" "Clojure")))))
 
-(deftest learning-list-test
+(deftest ^:task6 learning-list-test
   (is (= 3 (tracks-on-tracks-on-tracks/learning-list))))
