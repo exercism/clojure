e2b71ee5fca284546baaec1aa635f610f67f5a8e
porkostomus <porkostomus@mail.com>
2021-09-20 20:29:42 -0700

    add task metadata

--- exercises/concept/squeaky-clean/test/squeaky_clean_test.clj
+++ exercises/concept/squeaky-clean/test/squeaky_clean_test.clj
@@ -2,29 +2,29 @@
   (:require [clojure.test :refer [deftest testing is]]
             squeaky-clean))
 
-(deftest clean-single-letter
+(deftest ^:task1 clean-single-letter
   (is (= "A" (squeaky-clean/clean "A"))))
 
-(deftest clean-clean-string
+(deftest ^:task1 clean-clean-string
   (is (= "àḃç" (squeaky-clean/clean "àḃç"))))
 
-(deftest clean-string-with-spaces
+(deftest ^:task1 clean-string-with-spaces
   (is (= "my___Id" (squeaky-clean/clean "my   Id"))))
 
-(deftest clean-string-with-control-char
-  (is (= "myCTRLId" (squeaky-clean/clean "my\u0000Id"))))
-
-(deftest clean-string-with-no-letters
+(deftest ^:task1 clean-string-with-no-letters
   (is (= "" (squeaky-clean/clean "😀😀😀"))))
 
-(deftest clean-empty-string
+(deftest ^:task1 clean-empty-string
   (is (= "" (squeaky-clean/clean ""))))
 
-(deftest convert-kebab-to-camel-case
+(deftest ^:task2 clean-string-with-control-char
+  (is (= "myCTRLId" (squeaky-clean/clean "my\u0000Id"))))
+
+(deftest ^:task3 convert-kebab-to-camel-case
   (is (= "àḂç" (squeaky-clean/clean "à-ḃç"))))
 
-(deftest omit-lower-case-greek-letters
+(deftest ^:task4 omit-lower-case-greek-letters
   (is (= "MyΟFinder" (squeaky-clean/clean "MyΟβιεγτFinder"))))
 
-(deftest combine-conversions
+(deftest ^:task3 combine-conversions
   (is (= "_AbcĐCTRL" (squeaky-clean/clean "9 -abcĐ😀ω\0"))))
