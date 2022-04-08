e7878c9611724ced239f5d57e3992fdf77667484
Bobby Towers <porkostomus@mail.com>
2022-04-08 10:50:55 -0700

    add notice to explain the test runner (#469)

--- exercises/concept/lucians-luscious-lasagna/src/lucians_luscious_lasagna.clj
+++ exercises/concept/lucians-luscious-lasagna/src/lucians_luscious_lasagna.clj
@@ -1,3 +1,9 @@
+;; 👋🏽 Hi there! Welcome to the Clojure Track.
+;; The online test-runner is powered by babashka and the Small Clojure Interpreter (SCI).
+;; Almost all language features are supported, with the exception of low-level constructs
+;; like `deftype`, and certain Java classes. For more info, see:
+;; https://github.com/babashka/babashka#differences-with-clojure
+
 (ns lucians-luscious-lasagna)
 
 (def expected-time
