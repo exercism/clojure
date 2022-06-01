e2ddb9eec874242885843b21999e19c2e28d546c
Bobby Towers <btowers793@gmail.com>
2022-06-01 00:44:20 -0700

    Add date-parser learning exercise (#336)
    * add date-parser concept exercise
    
    * fix directory structure
    
    * add exercise config.json
    
    * add exercise to main config.json
    
    * fix json syntax error
    
    * add empty docs files
    
    * add blurb
    
    * create exemplar/stub files

---
+++ exercises/concept/date-parser/src/date_parser.clj
@@ -0,0 +1,39 @@
+(ns date-parser)
+
+(def day)
+(def month)
+(def year)
+
+(def days)
+
+(defn day-names [s]
+  )
+
+(def months)
+
+(defn month-names [s]
+  )
+
+(defn capture-month [s]
+  )
+
+(defn capture-day [s]
+  )
+
+(defn capture-year [s]
+  )
+
+(defn capture-month-name [s]
+  )
+
+(defn capture-day-name [s]
+  )
+
+(defn capture-numeric-date [s]
+  )
+
+(defn capture-month-name-date [s]
+  )
+
+(defn capture-day-month-name-date [s]
+  )
