239c845212cdddf60b908b9bb0e9f046282ec8cc
Bobby Towers <porkostomus@mail.com>
2022-06-08 00:18:18 -0700

    Update test.clj

--- test.clj
+++ test.clj
@@ -22,8 +22,8 @@
   []
   (as-path (System/getProperty "user.dir")))
 
-(def root (str (cwd) "/clojure/"))
-(def test-runner-dir (str (cwd) "/clojure/"))
+(def root (str (cwd) "/"))
+(def test-runner-dir (str (cwd) "/"))
 
 (defn- ->snake_case [s] (str/replace s \- \_))
 
