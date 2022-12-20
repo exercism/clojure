7f11e4bee92c3d94fa4d65b2eef7a2b6354fea50
Dave Rolle <44704707+miridius@users.noreply.github.com>
2022-12-20 04:54:10 +0100

    Add task-test metadata v2 (#501)
    * add task metadata
    
    * Switch to the {:task x} meta format used by the test runner
    
    See exercism/clojure-test-runner#43
    
    * Add test task ids for exercises with task numbers
    
    These were the only 2 left that weren't already done
    
    Co-authored-by: porkostomus <porkostomus@mail.com>
    Co-authored-by: Bobbi Towers <btowers793@gmail.com>

--- exercises/concept/elyses-destructured-enchantments/test/elyses_destructured_enchantments_test.clj
+++ exercises/concept/elyses-destructured-enchantments/test/elyses_destructured_enchantments_test.clj
@@ -2,41 +2,41 @@
   (:require [clojure.test :refer :all]
             [elyses-destructured-enchantments :refer :all]))
 
-(deftest first-card-single-test
+(deftest ^{:task 1} first-card-single-test
   (is (= 3 (first-card [3]))))
 
-(deftest first-card-multiple-test
+(deftest ^{:task 1} first-card-multiple-test
   (is (= 8 (first-card [8 3 9 5]))))
 
-(deftest second-card-2-test
+(deftest ^{:task 2} second-card-2-test
   (is (= 4 (second-card [10 4]))))
 
-(deftest second-card-4-test
+(deftest ^{:task 2} second-card-4-test
   (is (= 5 (second-card [2 5 1 6]))))
 
-(deftest second-card-empty-test
+(deftest ^{:task 2} second-card-empty-test
   (is (nil? (second-card []))))
 
-(deftest second-card-single-test
+(deftest ^{:task 2} second-card-single-test
   (is (nil? (second-card [8]))))
 
-(deftest swap-top-two-cards-2-test
+(deftest ^{:task 3} swap-top-two-cards-2-test
   (is (= [6 3] (swap-top-two-cards [3 6]))))
 
-(deftest swap-top-two-cards-5-test
+(deftest ^{:task 3} swap-top-two-cards-5-test
   (is (= [4 10 3 7 8] (swap-top-two-cards [10 4 3 7 8]))))
 
-(deftest discard-top-card-single-test
+(deftest ^{:task 4} discard-top-card-single-test
   (is (= [7 nil] (discard-top-card [7]))))
 
-(deftest discard-top-card-4-test
+(deftest ^{:task 4} discard-top-card-4-test
   (is (= [9 [2 10 4]] (discard-top-card [9 2 10 4]))))
 
-(deftest insert-face-cards-3-test
+(deftest ^{:task 5} insert-face-cards-3-test
   (is (= [3 "jack" "queen" "king" 10 7] (insert-face-cards [3 10 7]))))
 
-(deftest insert-face-cards-1-test
+(deftest ^{:task 5} insert-face-cards-1-test
   (is (= [9 "jack" "queen" "king"] (insert-face-cards [9]))))
 
-(deftest insert-face-cards-empty-test
+(deftest ^{:task 5} insert-face-cards-empty-test
   (is (= ["jack" "queen" "king"] (insert-face-cards []))))
--- exercises/concept/log-levels/test/log_levels_test.clj
+++ exercises/concept/log-levels/test/log_levels_test.clj
@@ -1,36 +1,36 @@
 (ns log-levels-test
-  (:require [clojure.test :refer [deftest testing is]]
+  (:require [clojure.test :refer [deftest is]]
             log-levels))
 
-(deftest message-error-test
+(deftest ^{:task 1} message-error-test
   (is (= "Stack overflow" (log-levels/message "[ERROR]: Stack overflow"))))
 
-(deftest message-warning-test
+(deftest ^{:task 1} message-warning-test
   (is (= (log-levels/message "[WARNING]: Disk almost full") "Disk almost full")))
 
-(deftest message-info-test
+(deftest ^{:task 1} message-info-test
   (is (= (log-levels/message "[INFO]: File moved") "File moved")))
 
-(deftest message-trim-whitespace-test
+(deftest ^{:task 1} message-trim-whitespace-test
   (is (= "Timezone not set" (log-levels/message "[WARNING]:   \tTimezone not set  \r\n"))))
 
-(deftest log-level-error-test
+(deftest ^{:task 2} log-level-error-test
   (is (= "error" (log-levels/log-level "[ERROR]: Disk full"))))
 
-(deftest log-level-warning-test
+(deftest ^{:task 2} log-level-warning-test
   (is (= "warning" (log-levels/log-level "[WARNING]: Unsafe password"))))
 
-(deftest log-level-info-test
+(deftest ^{:task 2} log-level-info-test
   (is (= "info" (log-levels/log-level "[INFO]: Timezone changed"))))
 
-(deftest reformat-error-test
+(deftest ^{:task 3} reformat-error-test
   (is (= "Segmentation fault (error)" (log-levels/reformat "[ERROR]: Segmentation fault"))))
 
-(deftest reformat-warning-test
+(deftest ^{:task 3} reformat-warning-test
   (is (= "Decreased performance (warning)" (log-levels/reformat "[WARNING]: Decreased performance"))))
 
-(deftest reformat-info-test
+(deftest ^{:task 3} reformat-info-test
   (is (= "Disk defragmented (info)" (log-levels/reformat "[INFO]: Disk defragmented"))))
 
-(deftest reformat-trim-whitespace-test
+(deftest ^{:task 3} reformat-trim-whitespace-test
   (is (= "Corrupt disk (error)" (log-levels/reformat "[ERROR]: \t Corrupt disk\t \t \r\n"))))
