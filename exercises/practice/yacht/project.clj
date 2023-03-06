8b3884d85982b18942a9d96b8da4a962ebd11ec1
Bobby Towers <porkostomus@mail.com>
2023-03-06 14:28:08 -0800

    init yacht exercise

---
+++ exercises/practice/yacht/project.clj
@@ -0,0 +1,4 @@
+(defproject yacht "0.1.0-SNAPSHOT"
+  :description "yacht exercise."
+  :url "https://github.com/exercism/clojure/tree/master/exercises/yacht"
+  :dependencies [[org.clojure/clojure "1.10.0"]])
