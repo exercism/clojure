5bde2c7a566489500e7339a394b124029a26f2e7
Bobby Towers <porkostomus@mail.com>
2023-03-06 15:16:59 -0800

    refactor example

--- exercises/practice/yacht/.meta/src/example.clj
+++ exercises/practice/yacht/.meta/src/example.clj
@@ -1,35 +1,34 @@
 (ns yacht)
 
-(defn getScoreForTheYachtCategory [dices]
+(defn yacht [dices]
   (if (= (count (set dices)) 1) 50 0))
 
-(defn mapDicesToCounterArray [dices]
-  (let [counterArray (atom [0 0 0 0 0 0])]
-    (doseq [item dices]
-      (swap! counterArray update (dec item) inc))
-    counterArray))
+(defn dice-frequencies [dices]
+  (mapv #(if (nil? %) 0 %)
+        (map (fn [v] (get (frequencies dices) v))
+             (range 1 7))))
 
-(defn getNoOfAppearances [dices diceValue]
-  (count (filter (fn [value] (= value diceValue)) dices)))
+(defn appearances [dices diceValue]
+  (get (dice-frequencies dices) (dec diceValue)))
 
 (defn getSumOfDices [dices]
   (reduce + 0 dices))
 
-(defn getScoreForTheFourOfAKindCategory [dices]
-  (let [counterArray @(mapDicesToCounterArray dices)
+(defn four-of-kind [dices]
+  (let [counterArray (dice-frequencies dices)
         scores (remove nil? (for [i (range 6)]
                               (when (>= (get counterArray i) 4)
                                 (* 4 (inc i)))))]
     (if (empty? scores) 0 (first scores))))
 
-(defn getScoreForTheLittleStraightCategory [dices]
+(defn little-straight [dices]
   (if (= (sort dices) [1 2 3 4 5]) 30 0))
 
-(defn getScoreForTheBigStraightCategory [dices]
+(defn big-straight [dices]
   (if (= (sort dices) [2 3 4 5 6]) 30 0))
 
-(defn getScoreForTheFullHouseCategory [dices]
-  (let [counterArray @(mapDicesToCounterArray dices)]
+(defn full-house [dices]
+  (let [counterArray (dice-frequencies dices)]
     (if (and (contains? (set counterArray) 2)
              (contains? (set counterArray) 3))
       (reduce + dices)
@@ -37,18 +36,16 @@
 
 (defn score [dices category]
   (case category
-    "yacht" (getScoreForTheYachtCategory dices)
-    "ones" (getNoOfAppearances dices 1)
-    "twos" (* 2 (getNoOfAppearances dices 2))
-    "threes" (* 3 (getNoOfAppearances dices 3))
-    "fours" (* 4 (getNoOfAppearances dices 4))
-    "fives" (* 5 (getNoOfAppearances dices 5))
-    "sixes" (* 6 (getNoOfAppearances dices 6))
-    "full house" (getScoreForTheFullHouseCategory dices)
-    "four of a kind" (getScoreForTheFourOfAKindCategory
-                      dices)
-    "little straight" (getScoreForTheLittleStraightCategory
-                       dices)
-    "big straight" (getScoreForTheBigStraightCategory dices)
+    "yacht" (yacht dices)
+    "ones" (appearances dices 1)
+    "twos" (* 2 (appearances dices 2))
+    "threes" (* 3 (appearances dices 3))
+    "fours" (* 4 (appearances dices 4))
+    "fives" (* 5 (appearances dices 5))
+    "sixes" (* 6 (appearances dices 6))
+    "full house" (full-house dices)
+    "four of a kind" (four-of-kind dices)
+    "little straight" (little-straight dices)
+    "big straight" (big-straight dices)
     "choice" (getSumOfDices dices)
     0))
