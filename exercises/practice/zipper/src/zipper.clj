eabcb2aee37fac9e7d480b90d5735c896825877c
Bobby Towers <porkostomus@mail.com>
2023-03-10 12:31:26 -0800

    implement left, right, and value

--- exercises/practice/zipper/src/zipper.clj
+++ exercises/practice/zipper/src/zipper.clj
@@ -3,32 +3,31 @@
 (defn to_zip [root]
   [root nil])
 
-(defn zipper [branch? children make-node root]
-  ^{:zip/branch? branch? :zip/children children :zip/make-node make-node}
-  [root nil])
-
 (defn node [loc] (loc 0))
 
 (defn branch? [loc]
-  ((:zip/branch? (meta loc)) (node loc)))
+  ((fn [x] (or (map? x) (map? (nth x 1)))) (node loc)))
 
 (defn children [loc]
   (if (branch? loc)
-    ((:zip/children (meta loc)) (node loc))
+    ((fn [x] (seq (if (map? x) x (nth x 1)))) (node loc))
     (throw (Exception. "called children on a leaf node"))))
 
 (defn make-node [loc node children]
-  ((:zip/make-node (meta loc)) node children))
+  ((fn [x children]
+     (if (map? x)
+       (into {} children)
+       (assoc x 1 (into {} children)))) node children))
 
 (defn down [loc]
   (when (branch? loc)
     (let [[node path] loc
           [c & cnext :as cs] (children loc)]
       (when cs
-        (with-meta [c {:l []
-                       :pnodes (if path (conj (:pnodes path) node) [node])
-                       :ppath path
-                       :r cnext}] (meta loc))))))
+        [c {:l []
+            :pnodes (if path (conj (:pnodes path) node) [node])
+            :ppath path
+            :r cnext}]))))
 
 (defn next [loc]
   (let [[node {l :l  [r & rnext :as rs] :r :as path}] loc]
@@ -40,25 +39,17 @@
     (when (and path (seq l))
       (with-meta [(peek l) (assoc path :l (pop l) :r (cons node r))] (meta loc)))))
 
-(defn path
-  "Returns a seq of nodes leading to this loc"
-  {:added "1.0"}
-  [loc]
+(defn path [loc]
   (:pnodes (loc 1)))
 
-(defn up
-  "Returns the loc of the parent of the node at this loc, or nil if at
-  the top"
-  {:added "1.0"}
-  [loc]
+(defn up [loc]
   (let [[node {l :l, ppath :ppath, pnodes :pnodes r :r, changed? :changed?, :as path}] loc]
     (when pnodes
       (let [pnode (peek pnodes)]
-        (with-meta (if changed?
-                     [(make-node loc pnode (concat l (cons node r)))
-                      (and ppath (assoc ppath :changed? true))]
-                     [pnode ppath])
-          (meta loc))))))
+        (if changed?
+          [(make-node loc pnode (concat l (cons node r)))
+           (and ppath (assoc ppath :changed? true))]
+          [pnode ppath])))))
 
 (defn to_tree [loc]
   (if (= :end (loc 1))
@@ -68,53 +59,43 @@
         (recur p)
         (node loc)))))
 
-(defn to_zip [m]
-  (zipper
-   (fn [x] (or (map? x) (map? (nth x 1))))
-   (fn [x] (seq (if (map? x) x (nth x 1))))
-   (fn [x children]
-     (if (map? x)
-       (into {} children)
-       (assoc x 1 (into {} children))))
-   m))
-
-
-
-
-(def m {:a 3 :b {:x true :y false} :c 4})
-
-  (map? m)
-
-(-> (to_zip m) down next node)
-  
-(def tree
-  {:value 1
-   :left  {:value 2
-           :left  nil,
-           :right {:value 3
-                   :left  nil
-                   :right nil}},
-   :right {:value 4
-           :left  nil
-           :right nil}})
-
-(down (to_zip tree))
-
-(defn left [tree]
-  (:left tree))
-
-(defn right [tree]
-  (:right tree))
-
-(defn value [tree]
-  (:value tree))
-
-(-> tree
-    left
-    left)
-
 (defn set_value [z x])
 
 (defn set_left [z x])
 
 (defn set_right [z x])
+
+(defn left 
+  "Takes a zipper with the focus at a tree node,
+   returns a new zipper navigated to its left child.
+   If no left child, returns nil."
+  [z]
+  (let [l (some #(when (= :left (ffirst %)) %) (iterate next (down z)))]
+    (when-not (nil? (first (nfirst l))) l)))
+
+(defn right
+  "Takes a zipper with the focus at a tree node,
+   returns a new zipper navigated to its right child.
+   If no right child, returns nil."
+  [z]
+  (let [r (some #(when (= :left (ffirst %)) %) (iterate next (down z)))]
+    (when-not (nil? (first (nfirst r))) r)))
+
+(defn value
+  "Takes a zipper with the focus at a tree node,
+   returns its value."
+  [z]
+  (first (nfirst 
+          (some #(when (= :value (ffirst %)) %) (iterate next (down z))))))
+
+(comment 
+  (def tree
+    {:value 1
+     :left  {:value 2
+             :left  nil,
+             :right {:value 3
+                     :left  nil
+                     :right nil}},
+     :right {:value 4
+             :left  nil
+             :right nil}}))
--- exercises/practice/zipper/test/zipper_test.clj
+++ exercises/practice/zipper/test/zipper_test.clj
@@ -1,94 +1,202 @@
 (ns zipper-test
-  (:require [clojure.test :refer [deftest testing is]]
+  (:require [clojure.test :refer [deftest testing is run-tests]]
              zipper))
 
+(def t1 {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}})
+
+(comment
+  (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}} 
+      zipper/to_zip  zipper/to_tree)
+  
+
+  (-> t1
+      zipper/to_zip
+      zipper/left
+      zipper/right
+      zipper/value)
+  
+  (-> t1
+      zipper/to_zip
+      zipper/left
+      zipper/left)
+  )
+
 (deftest expectedValue-test
-  (testing "data is retained"
-     (is (= {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}} 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           zipper/to_tree))))
-  (testing "left, right and value"
-     (is (= 3 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           zipper/left
-           zipper/right
-           zipper/value))))
-  (testing "dead end"
-     (is (= nil 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           zipper/left
-           zipper/left))))
-  (testing "tree from deep focus"
-     (is (= {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}} 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           zipper/left
-           zipper/right
-           zipper/to_tree))))
-  (testing "traversing up from top"
-     (is (= nil 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           zipper/up))))
-  (testing "left, right, and up"
-     (is (= 3 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           zipper/left
-           zipper/up
-           zipper/right
-           zipper/up
-           zipper/left
-           zipper/right
-           zipper/value))))
-  (testing "test ability to descend multiple levels and return"
-     (is (= 1 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           zipper/left
-           zipper/right
-           zipper/up
-           zipper/up
-           zipper/value))))
-  (testing "set_value"
-     (is (= {:value 1, :left {:value 5, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}} 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           zipper/left
-           (zipper/set_value 5)
-           zipper/to_tree))))
-  (testing "set_value after traversing up"
-     (is (= {:value 1, :left {:value 5, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}} 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           zipper/left
-           zipper/right
-           zipper/up
-           (zipper/set_value 5)
-           zipper/to_tree))))
-  (testing "set_left with leaf"
-     (is (= {:value 1, :left {:value 2, :left {:value 5, :left nil, :right nil}, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}} 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           zipper/left
-           (zipper/set_left {:value 5, :left nil, :right nil})
-           zipper/to_tree))))
-  (testing "set_right with null"
-     (is (= {:value 1, :left {:value 2, :left nil, :right nil}, :right {:value 4, :left nil, :right nil}} 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           zipper/left
-           (zipper/set_right nil)
-           zipper/to_tree))))
-  (testing "set_right with subtree"
-     (is (= {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 6, :left {:value 7, :left nil, :right nil}, :right {:value 8, :left nil, :right nil}}} 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           (zipper/set_right {:value 6, :left {:value 7, :left nil, :right nil}, :right {:value 8, :left nil, :right nil}})
-           zipper/to_tree))))
-  (testing "set_value on deep focus"
-     (is (= {:value 1, :left {:value 2, :left nil, :right {:value 5, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}} 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           zipper/left
-           zipper/right
-           (zipper/set_value 5)
-           zipper/to_tree)))))
+  (let [tree {:value 1
+              :left  {:value 2
+                      :left  nil
+                      :right {:value 3
+                              :left  nil
+                              :right nil}}
+              :right {:value 4
+                      :left  nil
+                      :right nil}}]
+    (testing "data is retained"
+      (is (= tree (-> tree zipper/to_zip zipper/to_tree))))
+    (testing "left, right and value"
+      (is (= 3 (-> tree
+                   zipper/to_zip
+                   zipper/left
+                   zipper/right
+                   zipper/value))))
+    (testing "dead end"
+      (is (= nil (-> tree
+                     zipper/to_zip
+                     zipper/left
+                     zipper/left))))
+    (testing "tree from deep focus"
+      (is (= tree (-> tree
+                      zipper/to_zip
+                      zipper/left
+                      zipper/right
+                      zipper/to_tree))))
+    (testing "traversing up from top"
+      (is (= nil 
+             (-> tree
+                 zipper/to_zip
+                 zipper/up))))
+    (testing "left, right, and up"
+      (is (= 3 
+             (-> tree
+                 zipper/to_zip
+                 zipper/left
+                 zipper/up
+                 zipper/right
+                 zipper/up
+                 zipper/left
+                 zipper/right
+                 zipper/value))))
+    (testing "test ability to descend multiple levels and return"
+      (is (= 1 
+             (-> tree
+                 zipper/to_zip
+                 zipper/left
+                 zipper/right
+                 zipper/up
+                 zipper/up
+                 zipper/value))))
+    (testing "set_value"
+      (is (= {:value 1
+              :left  {:value 5
+                      :left  nil
+                      :right {:value 3
+                              :left  nil
+                              :right nil}}
+              :right {:value 4
+                      :left  nil
+                      :right nil}} 
+             (-> tree
+                 zipper/to_zip
+                 zipper/left
+                 (zipper/set_value 5)
+                 zipper/to_tree))))
+    (testing "set_value after traversing up"
+      (is (= {:value 1
+              :left  {:value 5
+                      :left  nil
+                      :right {:value 3
+                              :left  nil
+                              :right nil}}
+              :right {:value 4
+                      :left  nil
+                      :right nil}} 
+             (-> tree
+                 zipper/to_zip
+                 zipper/left
+                 zipper/right
+                 zipper/up
+                 (zipper/set_value 5)
+                 zipper/to_tree))))
+    (testing "set_left with leaf"
+      (is (= {:value 1
+              :left  {:value 2
+                      :left  {:value 5
+                              :left  nil
+                              :right nil}
+                      :right {:value 3
+                              :left  nil
+                              :right nil}}
+              :right {:value 4
+                      :left  nil
+                      :right nil}} 
+             (-> tree
+                 zipper/to_zip
+                 zipper/left
+                 (zipper/set_left {:value 5
+                                   :left  nil
+                                   :right nil})
+                 zipper/to_tree))))
+    (testing "set_right with null"
+      (is (= {:value 1
+              :left  {:value 2
+                      :left  nil
+                      :right nil}
+              :right {:value 4
+                      :left  nil
+                      :right nil}} 
+             (-> tree
+                 zipper/to_zip
+                 zipper/left
+                 (zipper/set_right nil)
+                 zipper/to_tree))))
+    (testing "set_right with subtree"
+      (is (= {:value 1
+              :left  {:value 2
+                      :left  nil
+                      :right {:value 3
+                              :left  nil
+                              :right nil}}
+              :right {:value 6
+                      :left  {:value 7
+                              :left  nil
+                              :right nil}
+                      :right {:value 8
+                              :left  nil
+                              :right nil}}} 
+             (-> tree
+                 zipper/to_zip
+                 (zipper/set_right {:value 6
+                                    :left  {:value 7
+                                            :left  nil
+                                            :right nil}
+                                    :right {:value 8
+                                            :left  nil
+                                            :right nil}})
+                 zipper/to_tree))))
+    (testing "set_value on deep focus"
+      (is (= {:value 1
+              :left  {:value 2
+                      :left  nil
+                      :right {:value 5
+                              :left  nil
+                              :right nil}}
+              :right {:value 4
+                      :left  nil
+                      :right nil}} 
+             (-> tree
+                 zipper/to_zip
+                 zipper/left
+                 zipper/right
+                 (zipper/set_value 5)
+                 zipper/to_tree))))))
 
 (deftest sameResultFromOperations-test
   (testing "different paths to same zipper"
      (is (= nil 
-         (-> {:value 1, :left {:value 2, :left nil, :right {:value 3, :left nil, :right nil}}, :right {:value 4, :left nil, :right nil}}
-           zipper/left
-           zipper/up
-           zipper/right)))))
+         (-> {:value 1
+              :left  {:value 2
+                      :left  nil
+                      :right {:value 3
+                              :left  nil
+                              :right nil}}
+              :right {:value 4
+                      :left  nil
+                      :right nil}}
+             zipper/to_zip
+             zipper/left
+             zipper/up
+             zipper/right)))))
+
+(comment
+  (run-tests)
+  )
