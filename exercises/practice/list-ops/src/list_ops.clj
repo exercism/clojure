ce949bb72c7a6d1952e9bfab46331a9ef6dfd26f
Bobby Towers <porkostomus@mail.com>
2023-05-23 12:07:49 -0700

    init skeleton src file

---
+++ exercises/practice/list-ops/src/list_ops.clj
@@ -0,0 +1,25 @@
+(ns list-ops)
+
+(defn append []
+  )
+
+(defn concat []
+  )
+
+(defn filter []
+  )
+
+(defn length []
+  )
+
+(defn map []
+  )
+
+(defn foldl []
+  )
+
+(defn foldr []
+  )
+
+(defn reverse []
+  )
