3d9988fdd75720986d157077b2b36f41887342ba
Bobbi Towers <btowers793@gmail.com>
2023-06-03 11:25:59 -0700

    solve by maintaining a global set of active names

--- exercises/practice/robot-name/.meta/src/example.clj
+++ exercises/practice/robot-name/.meta/src/example.clj
@@ -1,53 +1,39 @@
-(ns robot-name
-  (:require [clojure.set :as set]))
+(ns robot-name)
 
-;; h/t next-mad-hatter: https://exercism.org/tracks/clojure/exercises/robot-name/solutions/next-mad-hatter
-;;
-;; First, note how allowed robot names can be viewed as
-;; mixed-base numbers (two digits in base 26, three in base 10)
-;; between 0 and what is called max-robot-code below.
-;;
-;; We'll call these numbers robot codes to differentiate them from
-;; their string representations (or "names").
-;;
-(def ^:private max-robot-code (+ (* 27 25 1000) 999))
-;;
-;; Above interpretation results in a mapping like this:
-;;
-;; (map code->name [0 1000 25999 26001 max-robot-code])
-;; => ("AA000" "AB000" "AZ999" "BA001" "ZZ999")
-;;
-(defn- code->name [n]
-  (let [part3   (mod n 1000)
-        part2   (mod (int (/ n 1000)) 26)
-        part1   (int (/ n 26000))
-        letter2 (char (+ (int \A) part2))
-        letter1 (char (+ (int \A) part1))]
-    (str letter1 letter2 (format "%03d" part3))))
-;;
-;; Our world contains last code taken along with the set of all currently taken
-;; codes.  Each robot will additionally hold all its previously assigned codes.
-;;
-(def ^:private world (atom [nil #{}]))
-(def ^:private all-codes (set (range (inc max-robot-code))))
-(defn- get-new-code [previous old-code [_ taken]]
-  (let [available (set/difference all-codes taken previous)
-        new-code  (-> available shuffle first)
-        taken'    (-> taken (conj new-code) (disj old-code))]
-    [new-code taken']))
-(defn robot-name [robot]
-  (-> robot :code deref code->name))
-(defn reset-name [robot]
-  (let [old-code     (-> robot :code deref)
-        [new-code _] (swap! world (partial get-new-code (-> robot :previous deref) (-> robot :code deref)))]
-    (assert (some? new-code))
-    (when old-code (swap! (:previous robot) #(conj % old-code)))
-    (reset! (:code robot) new-code)
-    robot))
+(defn letters [] 
+  (let [letter #(rand-nth (map char (range 65 91)))]
+    (apply str (repeatedly 2 letter))))
+
+(defn numbers []
+  (let [number #(rand-int 10)]
+    (apply str (repeatedly 3 number))))
+
+;; Maintain a set of robot names
+
+(def robots (atom #{}))
+
+;; Before returning robot, check if the set count has increased.
+
 (defn robot []
-  (-> {:previous (atom #{}) :code (atom nil)}
-      reset-name))
-(defn previous-names [robot]
-  (->> robot :previous deref (map code->name)))
-(defn current-names []
-  (->> @world second (map code->name)))
+  (let [active-names @robots
+        robot-count (count active-names)
+        new-name (str (letters) (numbers))]
+    (if (< robot-count (count (conj active-names new-name)))
+      (do (swap! robots conj new-name)
+          (atom {:name new-name}))
+      (recur))))
+
+(comment
+  (robot)
+  @robots
+  )
+
+(defn robot-name [robot]
+  (:name @robot))
+ 
+(defn reset-name [robot]
+  (swap! robot assoc :name (str (letters) (numbers))))
+
+(comment
+  (re-seq #"[A-Z]{2}\d{3}" (robot-name (robot)))
+  )
