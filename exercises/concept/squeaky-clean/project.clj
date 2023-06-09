4e6fca8edbc6b6613e90487af2ff778395f66b04
Bobby Towers <porkostomus@mail.com>
2023-06-09 09:17:16 -0700

    add leiningen config

---
+++ exercises/concept/squeaky-clean/project.clj
@@ -0,0 +1,4 @@
+(defproject squeaky-clean "0.1.0-SNAPSHOT"
+  :description "squeaky-clean exercise."
+  :url "https://github.com/exercism/clojure/tree/main/exercises/concept/squeaky-clean"
+  :dependencies [[org.clojure/clojure "1.11.1"]])
