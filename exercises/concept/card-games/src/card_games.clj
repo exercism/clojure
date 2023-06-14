c932f2522d91210f8b4f1bc9bb2d099bdd1a28b6
Bobby Towers <porkostomus@mail.com>
2023-06-13 20:46:43 -0700

    concat-rounds

--- exercises/concept/card-games/src/card_games.clj
+++ exercises/concept/card-games/src/card_games.clj
@@ -6,8 +6,15 @@
   [n]
   (list n (inc n) (+ n 2)))
 
+(defn concat-rounds 
+  "Takes two lists and returns a single `list` 
+   consisting of all the rounds in the first `list`, 
+   followed by all the rounds in the second `list`"
+  [l1 l2]
+  (concat l1 l2))
+
 (comment
   (map rounds '(0 1 10 27 99 666))
   (rounds 27)
-;;=> (27 28 29)
+  (concat-rounds '(27 28 29) '(35 36))
   )
--- exercises/concept/card-games/test/card_games_test.clj
+++ exercises/concept/card-games/test/card_games_test.clj
@@ -7,6 +7,16 @@
            (27 28 29) (99 100 101) (666 667 668)) 
          (map card-games/rounds '(0 1 10 27 99 666)))))
 
+(deftest concat-rounds-test
+  (is (= '(() (0 1) (1 2) (1 2)
+           (27 28 29 35 36)
+           (1 2 3 4 5 6))
+         (map #(apply card-games/concat-rounds %) '((() ()) ((0 1) ()) (() (1 2))
+                                         ((1) (2)) ((27 28 29) (35 36))
+                                         ((1 2 3) (4 5 6)))))))
+
 (comment
+  (apply card-games/concat-rounds '(() ()))
+
   (clojure.test/run-tests)
   )
