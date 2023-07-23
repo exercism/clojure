20d6c68d3607cbbf4a8880c16ec5095ba82229cc
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2023-07-23 05:06:00 +0300

    Remove non-required functions from phone-number.clj

--- exercises/practice/phone-number/src/phone_number.clj
+++ exercises/practice/phone-number/src/phone_number.clj
@@ -1,13 +1,6 @@
 (ns phone-number)
 
-(defn number [num-string] ;; <- arglist goes here
-      ;; your code goes here
-)
-
-(defn area-code [num-string] ;; <- arglist goes here
+(defn number
+  [] ;; <- arglist goes here
   ;; your code goes here
-)
-
-(defn pretty-print [num-string] ;; <- arglist goes here
-  ;; your code goes here
-)
+  )
