081276ccb716713452a977b171afffa21b192334
Kah Goh <villastar@yahoo.com.au>
2024-03-01 21:29:45 +0800

    Add list-ops exercise
    Co-authored-by: Bobby Towers <porkostomus@mail.com>

---
+++ exercises/practice/list-ops/.meta/src/example.clj
@@ -0,0 +1,33 @@
+(ns list-ops)
+
+(defn append [list1 list2] 
+  (list-ops/foldl (fn [acc elem] (conj acc elem)) list2 list1))
+
+(defn concat [list]
+  (cond
+    (empty? list) []
+    :else (list-ops/foldl (fn [acc elem] (list-ops/append acc elem)) (rest list) (first list))))
+
+(defn filter [f list]
+  (loop [acc [] l list]
+    (cond 
+      (empty? l) acc
+      (f (first l)) (recur (conj acc (first l)) (rest l))
+      :else (recur acc (rest l)))))
+
+(defn length [list]
+  (list-ops/foldl (fn [acc elem] (+ acc 1)) list 0))
+
+(defn map [f list]
+  (list-ops/foldl (fn [acc elem] (conj acc (f elem))) list []))
+(defn foldl [f list init]
+  (loop [acc init l list]
+    (cond
+      (empty? l) acc
+      :else (recur (f acc (first l)) (rest l)))))
+
+(defn foldr [f list init]
+  (list-ops/foldl f (list-ops/reverse list) init))
+
+(defn reverse [list]
+  (list-ops/foldl (fn [acc elem] (cons elem acc)) list []))
---
+++ exercises/practice/list-ops/src/list_ops.clj
@@ -0,0 +1,33 @@
+(ns list-ops)
+
+(defn append [list1 list2] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn concat [list] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn filter [f list] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn length [list] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn map [f list] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn foldl [f list init] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn foldr [f list init] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn reverse [list] ;; <- arglist goes here
+  ;; your code goes here
+)
