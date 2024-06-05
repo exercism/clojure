9e50cee21171fd4e5e34c19ae1a2f5b90967528c
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-06-05 16:47:39 +0300

    Binary search: Sync metadata docs, implement tests, update example solution (#643)
    * sync docs
    
    * sync metadata
    
    * implement tests
    
    * update example solution (no explicit recursion, passes new tests)

--- exercises/practice/binary-search/test/binary_search_test.clj
+++ exercises/practice/binary-search/test/binary_search_test.clj
@@ -1,40 +1,52 @@
 (ns binary-search-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest is testing]]
             binary-search))
 
-(def short-vector [1, 3, 4, 6, 8, 9, 11])
+(deftest finds-value-in-array-with-one-element
+  (testing "Finds a value in an array with one element"
+           (is (= 0 (binary-search/search-for 6 [6])))))
 
-(def large-vector [1, 3, 5, 8, 13, 21, 34, 55, 89])
+(deftest finds-value-in-middle-of-array
+  (testing "Finds a value in the middle of an array"
+    (is (= 3 (binary-search/search-for 6 [1, 3, 4, 6, 8, 9, 11])))))
 
-(def even-length-vector [1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377])
+(deftest finds-value-at-beginning-of-array
+  (testing "Finds a value at the beginning of an array"
+    (is (= 0 (binary-search/search-for 1 [1, 3, 4, 6, 8, 9, 11])))))
 
-(deftest it-finds-position-of-middle-item
-  (is (= 3 (binary-search/middle short-vector))))
+(deftest finds-value-at-end-of-array
+  (testing "Finds a value at the end of an array"
+    (is (= 6 (binary-search/search-for 11 [1, 3, 4, 6, 8, 9, 11])))))
 
-(deftest searches-a-singleton
-  (is (= 0 (binary-search/search-for 4 [4]))))
+(deftest finds-value-in-array-of-odd-length
+  (testing "Finds a value in an array of odd length"
+    (is (= 9 (binary-search/search-for 144 [1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634])))))
 
-(deftest it-finds-position-of-search-data
-  (is (= 5 (binary-search/search-for 9 short-vector))))
+(deftest finds-value-in-array-of-even-length
+  (testing "Finds a value in an array of even length"
+    (is (= 5 (binary-search/search-for 21 [1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377])))))
 
-(deftest it-finds-position-in-a-larger-list
-  (is (= 1 (binary-search/search-for 3 large-vector))))
+(deftest identifies-that-value-is-not-in-array
+  (testing "Identifies that a value is not included in the array"
+    (is (thrown-with-msg? Throwable #"not found"
+                          (binary-search/search-for 7 [1, 3, 4, 6, 8, 9, 11])))))
 
-(deftest it-finds-position-in-a-larger-list-again
-  (is (= 7 (binary-search/search-for 55 large-vector))))
+(deftest value-smaller-than-arrays-smallest-value-not-found
+  (testing "A value smaller than the array's smallest value is not found"
+    (is (thrown-with-msg? Throwable #"not found"
+                          (binary-search/search-for 0 [1, 3, 4, 6, 8, 9, 11])))))
 
-(deftest it-finds-correct-position-in-a-list-with-an-even-number-of-elements
-  (is (= 5 (binary-search/search-for 21 even-length-vector))))
+(deftest value-larger-than-arrays-largest-value-not-found
+  (testing "A value larger than the array's largest value is not found"
+    (is (thrown-with-msg? Throwable #"not found"
+                          (binary-search/search-for 13 [1, 3, 4, 6, 8, 9, 11])))))
 
-(deftest it-finds-correct-position-in-a-list-with-an-even-number-of-elements-again
-  (is (= 6 (binary-search/search-for 34 even-length-vector))))
+(deftest nothing-found-in-empty-array
+  (testing "Nothing is found in an empty array"
+    (is (thrown-with-msg? Throwable #"not found"
+                          (binary-search/search-for 1 [])))))
 
-(deftest it-works-on-lists
-  (is (= 7 (binary-search/search-for 7 (range 10)))))
-
-(deftest it-works-on-lists-again
-  (is (= 4 (binary-search/search-for 3 '(-3 -2 0 1 3 4)))))
-
-(deftest throws-exception-when-element-not-found
-  (is (thrown-with-msg? Throwable #"not found"
-                        (binary-search/search-for 20 short-vector))))
+(deftest nothing-found-when-left-and-right-bounds-cross
+  (testing "Nothing is found when the left and right bounds cross"
+    (is (thrown-with-msg? Throwable #"not found"
+                          (binary-search/search-for 0 [1, 2])))))
