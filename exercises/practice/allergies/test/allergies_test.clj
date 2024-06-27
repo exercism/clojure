88a8a71565f22832f575c8a77b0189c918967c15
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-06-28 01:32:53 +0300

    explicitly check for a true return value

--- exercises/practice/allergies/test/allergies_test.clj
+++ exercises/practice/allergies/test/allergies_test.clj
@@ -6,97 +6,97 @@
   (testing "Testing for eggs allergy -> not allergic to anything"
     (is (false? (allergies/allergic-to? 0 :eggs))))
   (testing "Testing for eggs allergy -> allergic only to eggs"
-    (is (allergies/allergic-to? 1 :eggs)))
+    (is (true? (allergies/allergic-to? 1 :eggs))))
   (testing "Testing for eggs allergy -> allergic to eggs and something else"
-    (is (allergies/allergic-to? 3 :eggs)))
+    (is (true? (allergies/allergic-to? 3 :eggs))))
   (testing "Testing for eggs allergy -> allergic to something, but not eggs"
     (is (false? (allergies/allergic-to? 2 :eggs))))
   (testing "Testing for eggs allergy -> allergic to everything"
-    (is (allergies/allergic-to? 255 :eggs))))
+    (is (true? (allergies/allergic-to? 255 :eggs)))))
 
 (deftest testing-for-peanuts-allergy
   (testing "Testing for peanuts allergy -> not allergic to anything"
     (is (false? (allergies/allergic-to? 0 :peanuts))))
   (testing "Testing for peanuts allergy -> allergic only to peanuts"
-    (is (allergies/allergic-to? 2 :peanuts)))
+    (is (true? (allergies/allergic-to? 2 :peanuts))))
   (testing "Testing for peanuts allergy -> allergic to peanuts and something else"
-    (is (allergies/allergic-to? 7 :peanuts)))
+    (is (true? (allergies/allergic-to? 7 :peanuts))))
   (testing "Testing for peanuts allergy -> allergic to something, but not peanuts"
     (is (false? (allergies/allergic-to? 5 :peanuts))))
   (testing "Testing for peanuts allergy -> allergic to everything"
-    (is (allergies/allergic-to? 255 :peanuts))))
+    (is (true? (allergies/allergic-to? 255 :peanuts)))))
 
 (deftest testing-for-shellfish-allergy
   (testing "Testing for shellfish allergy -> not allergic to anything"
     (is (false? (allergies/allergic-to? 0 :shellfish))))
   (testing "Testing for shellfish allergy -> allergic only to shellfish"
-    (is (allergies/allergic-to? 4 :shellfish)))
+    (is (true? (allergies/allergic-to? 4 :shellfish))))
   (testing "Testing for shellfish allergy -> allergic to shellfish and something else"
-    (is (allergies/allergic-to? 14 :shellfish)))
+    (is (true? (allergies/allergic-to? 14 :shellfish))))
   (testing "Testing for shellfish allergy -> allergic to something, but not shellfish"
     (is (false? (allergies/allergic-to? 10 :shellfish))))
   (testing "Testing for shellfish allergy -> allergic to everything"
-    (is (allergies/allergic-to? 255 :shellfish))))
+    (is (true? (allergies/allergic-to? 255 :shellfish)))))
 
 (deftest testing-for-strawberries-allergy
   (testing "Testing for strawberries allergy -> not allergic to anything"
     (is (false? (allergies/allergic-to? 0 :strawberries))))
   (testing "Testing for strawberries allergy -> allergic only to strawberries"
-    (is (allergies/allergic-to? 8 :strawberries)))
+    (is (true? (allergies/allergic-to? 8 :strawberries))))
   (testing "Testing for strawberries allergy -> allergic to strawberries and something else"
-    (is (allergies/allergic-to? 28 :strawberries)))
+    (is (true? (allergies/allergic-to? 28 :strawberries))))
   (testing "Testing for strawberries allergy -> allergic to something, but not strawberries"
     (is (false? (allergies/allergic-to? 20 :strawberries))))
   (testing "Testing for strawberries allergy -> allergic to everything"
-    (is (allergies/allergic-to? 255 :strawberries))))
+    (is (true? (allergies/allergic-to? 255 :strawberries)))))
 
 (deftest testing-for-tomatoes-allergy
   (testing "Testing for tomatoes allergy -> not allergic to anything"
     (is (false? (allergies/allergic-to? 0 :tomatoes))))
   (testing "Testing for tomatoes allergy -> allergic only to tomatoes"
-    (is (allergies/allergic-to? 16 :tomatoes)))
+    (is (true? (allergies/allergic-to? 16 :tomatoes))))
   (testing "Testing for tomatoes allergy -> allergic to tomatoes and something else"
-    (is (allergies/allergic-to? 56 :tomatoes)))
+    (is (true? (allergies/allergic-to? 56 :tomatoes))))
   (testing "Testing for tomatoes allergy -> allergic to something, but not tomatoes"
     (is (false? (allergies/allergic-to? 40 :tomatoes))))
   (testing "Testing for tomatoes allergy -> allergic to everything"
-    (is (allergies/allergic-to? 255 :tomatoes))))
+    (is (true? (allergies/allergic-to? 255 :tomatoes)))))
 
 (deftest testing-for-chocolate-allergy
   (testing "Testing for chocolate allergy -> not allergic to anything"
     (is (false? (allergies/allergic-to? 0 :chocolate))))
   (testing "Testing for chocolate allergy -> allergic only to chocolate"
-    (is (allergies/allergic-to? 32 :chocolate)))
+    (is (true? (allergies/allergic-to? 32 :chocolate))))
   (testing "Testing for chocolate allergy -> allergic to chocolate and something else"
-    (is (allergies/allergic-to? 112 :chocolate)))
+    (is (true? (allergies/allergic-to? 112 :chocolate))))
   (testing "Testing for chocolate allergy -> allergic to something, but not chocolate"
     (is (false? (allergies/allergic-to? 80 :chocolate))))
   (testing "Testing for chocolate allergy -> allergic to everything"
-    (is (allergies/allergic-to? 255 :chocolate))))
+    (is (true? (allergies/allergic-to? 255 :chocolate)))))
 
 (deftest testing-for-pollen-allergy
   (testing "Testing for pollen allergy -> not allergic to anything"
     (is (false? (allergies/allergic-to? 0 :pollen))))
   (testing "Testing for pollen allergy -> allergic only to pollen"
-    (is (allergies/allergic-to? 64 :pollen)))
+    (is (true? (allergies/allergic-to? 64 :pollen))))
   (testing "Testing for pollen allergy -> allergic to pollen and something else"
-    (is (allergies/allergic-to? 224 :pollen)))
+    (is (true? (allergies/allergic-to? 224 :pollen))))
   (testing "Testing for pollen allergy -> allergic to something, but not pollen"
     (is (false? (allergies/allergic-to? 160 :pollen))))
   (testing "Testing for pollen allergy -> allergic to everything"
-    (is (allergies/allergic-to? 255 :pollen))))
+    (is (true? (allergies/allergic-to? 255 :pollen)))))
 
 (deftest testing-for-cats-allergy
   (testing "Testing for cats allergy -> not allergic to anything"
     (is (false? (allergies/allergic-to? 0 :cats))))
   (testing "Testing for cats allergy -> allergic only to cats"
-    (is (allergies/allergic-to? 128 :cats)))
+    (is (true? (allergies/allergic-to? 128 :cats))))
   (testing "Testing for cats allergy -> allergic to cats and something else"
-    (is (allergies/allergic-to? 192 :cats)))
+    (is (true? (allergies/allergic-to? 192 :cats))))
   (testing "Testing for cats allergy -> allergic to something, but not cats"
     (is (false? (allergies/allergic-to? 64 :cats))))
   (testing "Testing for cats allergy -> allergic to everything"
-    (is (allergies/allergic-to? 255 :cats))))
+    (is (true? (allergies/allergic-to? 255 :cats)))))
 
 (deftest list-when
   (testing "List when: no allergies"
