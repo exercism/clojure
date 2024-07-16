d618852399a1fc5cdfea84adeda67d6fb5aa65a0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-07-16 21:11:52 +0300

    scrabble-score: Implement tests

--- exercises/practice/scrabble-score/test/scrabble_score_test.clj
+++ exercises/practice/scrabble-score/test/scrabble_score_test.clj
@@ -1,21 +1,47 @@
 (ns scrabble-score-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest testing is]]
             scrabble-score))
 
-(deftest lower-case-letter
-  (is (= 1 (scrabble-score/score-letter "a"))))
+(deftest lowercase-letter
+  (testing "Lowercase letter"
+    (is (= 1 (scrabble-score/score-word "a")))))
 
-(deftest upper-case-letter
-  (is (= 1 (scrabble-score/score-letter "A"))))
+(deftest uppercase-letter
+  (testing "Uppercase letter"
+    (is (= 1 (scrabble-score/score-word "A")))))
 
-(deftest two-letter-word
-  (is (= 2 (scrabble-score/score-word "at"))))
+(deftest valuable-letter
+  (testing "Valuable letter"
+    (is (= 4 (scrabble-score/score-word "f")))))
 
-(deftest bigger-word-1
-  (is (= 6 (scrabble-score/score-word "street"))))
+(deftest short-word
+  (testing "Short word"
+    (is (= 2 (scrabble-score/score-word "at")))))
 
-(deftest bigger-word-2
-  (is (= 22 (scrabble-score/score-word "quirky"))))
+(deftest short-valuable-word
+  (testing "Short, valuable word"
+    (is (= 12 (scrabble-score/score-word "zoo")))))
 
-(deftest all-upper-case-word
-  (is (= 41 (scrabble-score/score-word "OXYPHENBUTAZONE"))))
+(deftest medium-word
+  (testing "Medium word"
+    (is (= 6 (scrabble-score/score-word "street")))))
+
+(deftest medium-valuable-word
+  (testing "medium, valuable word"
+    (is (= 22 (scrabble-score/score-word "quirky")))))
+
+(deftest long-mixed-case-word
+  (testing "Long, mixed-case word"
+    (is (= 41 (scrabble-score/score-word "OxyphenButazone")))))
+
+(deftest english-like-word
+  (testing "English-like word"
+    (is (= 8 (scrabble-score/score-word "pinata")))))
+
+(deftest empty-input
+  (testing "Empty input"
+    (is (= 0 (scrabble-score/score-word "")))))
+
+(deftest entire-alphabet-available
+  (testing "Entire alphabet available"
+    (is (= 87 (scrabble-score/score-word "abcdefghijklmnopqrstuvwxyz")))))
