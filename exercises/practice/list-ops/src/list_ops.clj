beedaca5b07ad490a8d90bb4c896c572b2059a8a
Kah Goh <villastar@yahoo.com.au>
2024-09-28 17:21:39 +0800

    Change list references to collection
    Co-authored-by: Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>

--- exercises/practice/list-ops/src/list_ops.clj
+++ exercises/practice/list-ops/src/list_ops.clj
@@ -1,48 +1,49 @@
 (ns list-ops)
 
 (defn append 
-  "Given two lists, adds all the items in the second list to the end of the first list"
+  "Given two collections, it adds all the items in the second collection to the end of the first collection"
   [coll1 coll2]
   ;; your code goes here
 )
 
 (defn concatenate 
-  "Given a series of lists, combines all the lists into one flattened list"
-  [coll]
+  "Given a series of collections, it combines all the collections into one flattened collection"
+  [colls]
   ;; your code goes here
 )
 
 (defn select-if
-  "Given a predicate and a list, returns the list of all items for which `predicate(item)` is True"
+  "Given a predicate and a collection, it returns the collection of all items for which predicate(item) is true"
   [pred coll]
   ;; your code goes here
 )
 
 (defn length 
-  "Given a list, returns the number of items within it"
+  "Given a collection, it returns the number of items within it"
   [coll]
   ;; your code goes here
 )
 
 (defn apply-to-each 
-  "Given a function and a list, returns the list of the results of applying `function(item)` on all items"
+  "Given a function and a collection, it returns the collection of the results of applying function(item) on all items"
   [f coll]
   ;; your code goes here
 )
 
 (defn foldl 
-  "Given a function, a list, and initial accumulator, folds (reduces) each item into the accumulator from the left"
+  "Given a function, a collection, and initial accumulator, it folds (reduces) each item into the accumulator from the left"
   [f coll init]
   ;; your code goes here
 )
 
-(defn foldr [f coll init]
-  "Given a function, a list, and an initial accumulator, folds (reduces) each item into the accumulator from the right"
+(defn foldr
+  "Given a function, a collection, and an initial accumulator, it folds (reduces) each item into the accumulator from the right"
+  [f coll init]
   ;; your code goes here
 )
 
 (defn reverse-order 
-  "Given a list, returns a list with all the original items, but in reverse order"
+  "Given a collection, it returns a collection with all the original items, but in reverse order"
   [coll]
   ;; your code goes here
 )
