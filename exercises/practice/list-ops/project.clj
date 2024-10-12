fcef9da834031ef60147a67d19ad0b1eff0d8b71
Kah Goh <villastar@yahoo.com.au>
2024-10-12 16:20:41 +0800

    Add list-ops exercise (WIP) (#623)
    * Add list-ops exercise
    
    Co-authored-by: Bobby Towers <porkostomus@mail.com>
    
    * Fix link to example
    
    * Add prerequisites & trim whitespace
    
    * Fix list-ops test
    
    * Add back generator.clj and remove unneeded comments from list_ops.clj
    
    * Rename list functions
    
    * Change list references to collection
    
    Co-authored-by: Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
    
    * Change references to vectors
    
    * Apply suggestions from code review
    
    Co-authored-by: Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
    
    * Add instructions.append
    
    * also change 'series' to 'vector'
    
    * Add tasxatzial as contributer
    
    ---------
    
    Co-authored-by: Bobby Towers <porkostomus@mail.com>
    Co-authored-by: Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>

---
+++ exercises/practice/list-ops/project.clj
@@ -0,0 +1,4 @@
+(defproject list-ops "0.1.0-SNAPSHOT"
+  :description "list-ops exercise."
+  :url "https://github.com/exercism/clojure/tree/main/exercises/list-ops"
+  :dependencies [[org.clojure/clojure "1.10.0"]])
