89e701169f6222216eff53d9d05ba215fa00285b
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-10-24 10:09:40 +0300

    bird-watcher: Update exemplar solution (#687)

--- exercises/concept/bird-watcher/.meta/exemplar.clj
+++ exercises/concept/bird-watcher/.meta/exemplar.clj
@@ -3,13 +3,13 @@
 (def last-week [0 2 5 3 7 8 4])
 
 (defn today [birds]
-  (last birds))
+  (peek birds))
 
 (defn inc-bird [birds]
   (update birds (dec (count birds)) inc))
 
 (defn day-without-birds? [birds]
-  (pos? (count (filter zero? birds))))
+  (not (every? pos? birds)))
 
 (defn n-days-count [birds n]
   (reduce + (take n birds)))
