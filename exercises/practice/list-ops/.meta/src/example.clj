09da2ba085c421ec8eba15b4c3c7febda0daa92f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-11-03 00:40:03 +0200

    rewrite example solution

--- exercises/practice/list-ops/.meta/src/example.clj
+++ exercises/practice/list-ops/.meta/src/example.clj
@@ -1,34 +1,50 @@
 (ns list-ops)
 
-(defn foldl [f coll init]
-  (loop [acc init l coll]
-    (cond
-      (empty? l) acc
-      :else (recur (f acc (first l)) (rest l)))))
+(declare foldl)
+(declare reverse-order)
 
-(defn append [coll1 coll2]
-  (list-ops/foldl (fn [acc elem] (conj acc elem)) coll2 coll1))
+(defn append
+  [coll1 coll2]
+  (foldl conj coll2 coll1))
 
-(defn concatenate [coll]
-  (cond
-    (empty? coll) []
-    :else (list-ops/foldl (fn [acc elem] (list-ops/append acc elem)) (rest coll) (first coll))))
+(defn concatenate
+  [colls]
+  (foldl append colls []))
 
-(defn select-if [pred coll]
-  (loop [acc [] l coll]
-    (cond
-      (empty? l) acc
-      (pred (first l)) (recur (conj acc (first l)) (rest l))
-      :else (recur acc (rest l)))))
+(defn select-if
+  [pred coll]
+  (let [reducer (fn [acc el]
+                  (if (pred el)
+                    (conj acc el)
+                    acc))]
+    (foldl reducer coll [])))
 
-(defn length [coll]
-  (list-ops/foldl (fn [acc elem] (+ acc 1)) coll 0))
+(defn length
+  [coll]
+  (let [reducer (fn [acc _]
+                  (inc acc))]
+    (foldl reducer coll 0)))
 
-(defn apply-to-each [f coll]
-  (list-ops/foldl (fn [acc elem] (conj acc (f elem))) coll []))
+(defn apply-to-each
+  [f coll]
+  (let [reducer (fn [acc el]
+                  (conj acc (f el)))]
+    (foldl reducer coll [])))
 
-(defn reverse-order [coll]
-  (list-ops/foldl (fn [acc elem] (cons elem acc)) coll []))
+(defn foldl
+  [f coll acc]
+  (if (seq coll)
+    (recur f (rest coll) (f acc (first coll)))
+    acc))
 
-(defn foldr [f coll init]
-  (list-ops/foldl f (list-ops/reverse-order coll) init))
+(defn foldr
+  [f coll acc]
+  (foldl f (reverse-order coll) acc))
+
+(defn reverse-order
+  [coll]
+  (loop [index (dec (length coll))
+         result []]
+    (if (neg? index)
+      result
+      (recur (dec index) (conj result (coll index))))))
