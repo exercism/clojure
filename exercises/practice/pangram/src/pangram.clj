dc0561163feaf9deeeccbf0683621a659ab66842
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-11-02 23:26:59 +0200

    update starter file

--- exercises/practice/pangram/src/pangram.clj
+++ exercises/practice/pangram/src/pangram.clj
@@ -1,5 +1,7 @@
 (ns pangram)
 
-(defn pangram? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn pangram?
+  "Returns true if the given string is a pangram; otherwise, returns false"
+  [s]
+  ;; function body
+  )
