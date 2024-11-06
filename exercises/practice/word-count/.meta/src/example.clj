ccfe4f8cac0899225a979eacb7df92f161312e8d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-11-06 23:02:33 +0200

    add an example solution that passes the tests

--- exercises/practice/word-count/.meta/src/example.clj
+++ exercises/practice/word-count/.meta/src/example.clj
@@ -1,19 +1,8 @@
-(ns word-count
-  (:require [clojure.string :refer [lower-case split]]))
+(ns word-count)
 
 (defn word-count
-  "return a hash of unique words and how many times they appeared in the input string"
-  [input]
-  (->> (split input #"\W+")
-       (map lower-case)
-       (group-by identity)
-       (reduce (fn [acc [word occurrences]]
-                 (assoc acc word (count occurrences))) {})))
-
-
-;; Another approach
-(defn word-count [s]
-  (->> s
-       lower-case
-       (re-seq #"\w+")
+  [phrase]
+  (->> phrase
+       clojure.string/lower-case
+       (re-seq #"\b\w+'?\w*\b")
        frequencies))
