4339766b6b448965a8ce1ea57c7cc5d2f5524f43
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-11-09 13:46:40 +0200

    update starter file

--- exercises/practice/isogram/src/isogram.clj
+++ exercises/practice/isogram/src/isogram.clj
@@ -1,5 +1,7 @@
 (ns isogram)
 
-(defn isogram? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn isogram?
+  "Returns true if the given string is an isogram; otherwise, returns false"
+  [s]
+  ;; function body
+  )
