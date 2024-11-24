9477b744768a9feee8697697a0dc4294c3a38b80
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-11-24 22:12:17 +0200

    refactor example solution

--- exercises/practice/transpose/.meta/src/example.clj
+++ exercises/practice/transpose/.meta/src/example.clj
@@ -2,24 +2,20 @@
 
 (defn pad-lines
   [lines]
-  (if-let [rev-lines (seq (reverse lines))]
-    (loop [reversed-lines (rest rev-lines)
-           padded-lines [(first rev-lines)]]
-      (if (seq reversed-lines)
-        (let [diff (- (.length (peek padded-lines)) (.length (first reversed-lines)))]
-          (if (pos? diff)
-            (recur (rest reversed-lines) (conj padded-lines (str (first reversed-lines) (apply str (repeat diff " ")))))
-            (recur (rest reversed-lines) (conj padded-lines (first reversed-lines)))))
-        (reverse padded-lines)))
+  (if-let [reversed-lines (seq (reverse lines))]
+    (loop [rev-lines (rest reversed-lines)
+           padded-lines [(first reversed-lines)]]
+      (if (seq rev-lines)
+        (let [diff (- (.length (peek padded-lines)) (.length (first rev-lines)))
+              new-padded-line (str (first rev-lines) (apply str (repeat diff " ")))]
+          (recur (rest rev-lines) (conj padded-lines new-padded-line)))
+        (rseq padded-lines)))
     []))
-
 
 (defn transpose
   [s]
-  (let [padded-lines (-> s clojure.string/split-lines pad-lines)]
-    (loop [result []
-           padded-lines padded-lines]
-      (if (seq (first padded-lines))
-        (recur (conj result (map first padded-lines))
-               (map rest padded-lines))
-        (clojure.string/join "\n" (map #(apply str %) result))))))
+  (loop [result []
+         padded-lines (-> s clojure.string/split-lines pad-lines)]
+    (if (seq (first padded-lines))
+      (recur (conj result (map first padded-lines)) (map rest padded-lines))
+      (clojure.string/join "\n" (map #(apply str %) result)))))
