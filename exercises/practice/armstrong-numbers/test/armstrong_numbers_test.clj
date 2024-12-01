9fda9cd12b02d442009be69522de21b85c1c733b
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-12-01 19:25:03 +0200

    include previously excluded tests

--- exercises/practice/armstrong-numbers/test/armstrong_numbers_test.clj
+++ exercises/practice/armstrong-numbers/test/armstrong_numbers_test.clj
@@ -37,3 +37,11 @@
 (deftest not-armstrong-number-9926314
   (testing "Seven-digit number that is not an Armstrong number"
     (is (false? (armstrong-numbers/armstrong? 9926314)))))
+
+(deftest armstrong-number-186709961001538790100634132976990
+  (testing "Armstrong number containing seven zeroes"
+    (is (true? (armstrong-numbers/armstrong? 186709961001538790100634132976990)))))
+
+(deftest armstrong-number-115132219018763992565095597973971522401
+  (testing "The largest and last Armstrong number"
+    (is (true? (armstrong-numbers/armstrong? 115132219018763992565095597973971522401)))))
