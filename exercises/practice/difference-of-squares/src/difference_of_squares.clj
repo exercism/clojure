2a3732ba3baf3d6d50c87d965863c4c611033b39
David Sletten <david@bosatsu.net>
2024-12-10 01:10:35 -0500

    Update difference_of_squares.clj
    The definition of the `difference` function must come last due to Clojure's single-pass compiler.

--- exercises/practice/difference-of-squares/src/difference_of_squares.clj
+++ exercises/practice/difference-of-squares/src/difference_of_squares.clj
@@ -1,13 +1,13 @@
 (ns difference-of-squares)
 
-(defn difference [] ;; <- arglist goes here
-  ;; your code goes here
-)
-
 (defn sum-of-squares [] ;; <- arglist goes here
   ;; your code goes here
 )
 
 (defn square-of-sum [] ;; <- arglist goes here
+  ;; your code goes here
+)
+
+(defn difference [] ;; <- arglist goes here
   ;; your code goes here
 )
