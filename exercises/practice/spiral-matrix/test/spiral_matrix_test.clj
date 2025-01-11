ae9c914e9437b9d588fe150058699b0a08f57669
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-11 04:40:55 +0200

    spiral-matrix: rename test functions

--- exercises/practice/spiral-matrix/test/spiral_matrix_test.clj
+++ exercises/practice/spiral-matrix/test/spiral_matrix_test.clj
@@ -2,30 +2,30 @@
   (:require [clojure.test :refer [deftest is testing]]
             spiral-matrix))
 
-(deftest test-8f584201-b446-4bc9-b132-811c8edd9040
+(deftest spiral_test_1
   (testing "empty spiral"
     (is (= []
            (spiral-matrix/spiral 0)))))
 
-(deftest test-e40ae5f3-e2c9-4639-8116-8a119d632ab2
+(deftest spiral_test_2
   (testing "trivial spiral"
     (is (= [[1]]
            (spiral-matrix/spiral 1)))))
 
-(deftest test-cf05e42d-eb78-4098-a36e-cdaf0991bc48
+(deftest spiral_test_3
   (testing "spiral of size 2"
     (is (= [[1 2]
             [4 3]]
            (spiral-matrix/spiral 2)))))
 
-(deftest test-1c475667-c896-4c23-82e2-e033929de939
+(deftest spiral_test_4
   (testing "spiral of size 3"
     (is (= [[1 2 3]
             [8 9 4]
             [7 6 5]]
            (spiral-matrix/spiral 3)))))
 
-(deftest test-05ccbc48-d891-44f5-9137-f4ce462a759d
+(deftest spiral_test_5
   (testing "spiral of size 4"
     (is (= [[1  2  3  4]
             [12 13 14 5]
@@ -33,7 +33,7 @@
             [10 9  8  7]]
            (spiral-matrix/spiral 4)))))
 
-(deftest test-f4d2165b-1738-4e0c-bed0-c459045ae50d
+(deftest spiral_test_6
   (testing "spiral of size 5"
     (is (= [[1  2  3  4  5]
             [16 17 18 19 6]
