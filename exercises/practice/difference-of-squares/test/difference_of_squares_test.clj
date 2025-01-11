f0192a5408df5da152cd7effe2a848df123cbef8
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-11 16:10:33 +0200

    difference-of-squares: Sync tests (#696)
    * sync tests
    
    * implement tests
    
    * update starter file
    
    * update config
    
    * rename test functions
    
    [no important files changed]

--- exercises/practice/difference-of-squares/test/difference_of_squares_test.clj
+++ exercises/practice/difference-of-squares/test/difference_of_squares_test.clj
@@ -1,30 +1,39 @@
 (ns difference-of-squares-test
-  (:require [clojure.test :refer [deftest is]]
-            [difference-of-squares :as dos]))
+  (:require [clojure.test :refer [deftest testing is]]
+            difference-of-squares))
 
-(deftest square-of-sum-to-5
-  (is (= 225 (dos/square-of-sum 5))))
+(deftest square-of-sum_test_1
+  (testing "Square the sum of the numbers up to the given number -> square of sum 1"
+    (is (= 1 (difference-of-squares/square-of-sum 1)))))
 
-(deftest sum-of-squares-to-5
-  (is (= 55 (dos/sum-of-squares 5))))
+(deftest square-of-sum_test_2
+  (testing "Square the sum of the numbers up to the given number -> square of sum 5"
+    (is (= 225 (difference-of-squares/square-of-sum 5)))))
 
-(deftest difference-of-squares-to-5
-  (is (= 170 (dos/difference 5))))
+(deftest square-of-sum_test_3
+  (testing "Square the sum of the numbers up to the given number -> square of sum 100"
+    (is (= 25502500 (difference-of-squares/square-of-sum 100)))))
 
-(deftest square-of-sum-to-10
-  (is (= 3025 (dos/square-of-sum 10))))
+(deftest sum-of-squares_test_1
+  (testing "Sum the squares of the numbers up to the given number -> sum of squares 1"
+    (is (= 1 (difference-of-squares/sum-of-squares 1)))))
 
-(deftest sum-of-squares-to-10
-  (is (= 385 (dos/sum-of-squares 10))))
+(deftest sum-of-squares_test_2
+  (testing "Sum the squares of the numbers up to the given number -> sum of squares 5"
+    (is (= 55 (difference-of-squares/sum-of-squares 5)))))
 
-(deftest difference-of-squares-to-10
-  (is (= 2640 (dos/difference 10))))
+(deftest sum-of-squares_test_3
+  (testing "Sum the squares of the numbers up to the given number -> sum of squares 100"
+    (is (= 338350 (difference-of-squares/sum-of-squares 100)))))
 
-(deftest square-of-sum-to-100
-  (is (= 25502500 (dos/square-of-sum 100))))
+(deftest difference_test_1
+  (testing "Subtract sum of squares from square of sums -> difference of squares 1"
+    (is (= 0 (difference-of-squares/difference 1)))))
 
-(deftest sum-of-squares-to-100
-  (is (= 338350 (dos/sum-of-squares 100))))
+(deftest difference_test_2
+  (testing "Subtract sum of squares from square of sums -> difference of squares 5"
+    (is (= 170 (difference-of-squares/difference 5)))))
 
-(deftest difference-of-squares-to-100
-  (is (= 25164150 (dos/difference 100))))
+(deftest difference_test_3
+  (testing "Subtract sum of squares from square of sums -> difference of squares 100"
+    (is (= 25164150 (difference-of-squares/difference 100)))))
