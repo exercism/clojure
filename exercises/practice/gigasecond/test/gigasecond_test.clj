5d5916d2deac2977efecbdf4cd0e5d2aa8fcc65a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-11 02:29:03 +0200

    rename test functions, make test descriptions identical to the ones from .toml

--- exercises/practice/gigasecond/test/gigasecond_test.clj
+++ exercises/practice/gigasecond/test/gigasecond_test.clj
@@ -2,20 +2,20 @@
   (:require [clojure.test :refer [deftest testing is]]
             gigasecond))
 
-(deftest test-92fbe71c-ea52-4fac-bd77-be38023cacf7
-  (testing "Date-only specification of time"
+(deftest from_test_1
+  (testing "date-only specification of time"
     (is (= [2043 1 1] (gigasecond/from 2011 4 25)))))
 
-(deftest test-6d86dd16-6f7a-47be-9e58-bb9fb2ae1433
-  (testing "Second test for date-only specification of time"
+(deftest from_test_2
+  (testing "second test for date-only specification of time"
     (is (= [2009 2 19] (gigasecond/from 1977 6 13)))))
 
-(deftest test-77eb8502-2bca-4d92-89d9-7b39ace28dd5
-  (testing "Third test for date-only specification of time"
+(deftest from_test_3
+  (testing "third test for date-only specification of time"
     (is (= [1991 3 27] (gigasecond/from 1959 7 19)))))
 
-(deftest test-fcec307c-7529-49ab-b0fe-20309197618a
-  (testing "Does not mutate the input"
+(deftest from_test_4
+  (testing "does not mutate the input"
     (let [date [1959 7 19]
           new-date (apply gigasecond/from date)]
       (is (= [1959 7 19] date)))))
