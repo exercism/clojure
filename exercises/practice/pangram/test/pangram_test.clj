17031f95f883aba8836ef62d05ff5f11d8695d75
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-11 16:11:35 +0200

    pangram: Sync tests (#701)
    * update starter file
    
    * sync tests
    
    * implement tests
    
    * update config
    
    * rename test functions, make test descriptions identical to the ones from .toml
    
    [no important files changed]

--- exercises/practice/pangram/test/pangram_test.clj
+++ exercises/practice/pangram/test/pangram_test.clj
@@ -1,38 +1,43 @@
 (ns pangram-test
-  (:require [clojure.test :refer [is deftest]]
-            [pangram :refer [pangram?]]))
+  (:require [clojure.test :refer [deftest testing is]]
+            pangram))
 
-(deftest empty-sentence
-  (is (false? (pangram? ""))))
+(deftest pangram?_test_1
+  (testing "empty sentence"
+    (is (false? (pangram/pangram? "")))))
 
-(deftest lowercase-pangram
-  (is (pangram? "the quick brown fox jumps over the lazy dog")))
+(deftest pangram?_test_2
+  (testing "perfect lower case"
+    (is (true? (pangram/pangram? "abcdefghijklmnopqrstuvwxyz")))))
 
-(deftest missing-character-x
-  (is
-   (false?
-    (pangram? "a quick movement of the enemy will jeopardize five gunboats"))))
+(deftest pangram?_test_3
+  (testing "only lower case"
+    (is (true? (pangram/pangram? "the quick brown fox jumps over the lazy dog")))))
 
-(deftest another-missing-character-x
-  (is
-   (false?
-    (pangram? "the quick brown fish jumps over the lazy dog"))))
+(deftest pangram?_test_4
+  (testing "missing the letter 'x'"
+    (is (false? (pangram/pangram? "a quick movement of the enemy will jeopardize five gunboats")))))
 
-(deftest with-underscores
-  (is (pangram? "the_quick_brown_fox_jumps_over_the_lazy_dog")))
+(deftest pangram?_test_5
+  (testing "missing the letter 'h'"
+    (is (false? (pangram/pangram? "five boxing wizards jump quickly at it")))))
 
-(deftest with-numbers
-  (is (pangram? "the 1 quick brown fox jumps over the 2 lazy dogs")))
+(deftest pangram?_test_6
+  (testing "with underscores"
+    (is (true? (pangram/pangram? "the_quick_brown_fox_jumps_over_the_lazy_dog")))))
 
-(deftest missing-letters-replaced-by-numbers
-  (is
-   (false?
-    (pangram? "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"))))
+(deftest pangram?_test_7
+  (testing "with numbers"
+    (is (true? (pangram/pangram? "the 1 quick brown fox jumps over the 2 lazy dogs")))))
 
-(deftest mixed-case-and-punctuation
-  (is (pangram? "\"Five quacking Zephyrs jolt my wax bed.\"")))
+(deftest pangram?_test_8
+  (testing "missing letters replaced by numbers"
+    (is (false? (pangram/pangram? "7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog")))))
 
-(deftest upper-and-lower-not-counted-separately
-  (is
-   (false?
-    (pangram? "the quick brown fox jumps over with lazy FX"))))
+(deftest pangram?_test_9
+  (testing "mixed case and punctuation"
+    (is (true? (pangram/pangram? "\"Five quacking Zephyrs jolt my wax bed.\"")))))
+
+(deftest pangram?_test_10
+  (testing "a-m and A-M are 26 different characters but not a pangram"
+    (is (false? (pangram/pangram? "abcdefghijklm ABCDEFGHIJKLM")))))
