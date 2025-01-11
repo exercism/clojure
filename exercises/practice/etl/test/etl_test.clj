3a7e6d3ccb6594b919e317177270fb46699e2ea6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-11 02:25:53 +0200

    rename test functions, make test descriptions identical to the ones from .toml

--- exercises/practice/etl/test/etl_test.clj
+++ exercises/practice/etl/test/etl_test.clj
@@ -2,23 +2,23 @@
   (:require [clojure.test :refer [deftest testing is]]
             etl))
 
-(deftest test-78a7a9f9-4490-4a47-8ee9-5a38bb47d28f
-  (testing "Single letter"
+(deftest transform_test_1
+  (testing "single letter"
     (is (= {"a" 1}
            (etl/transform {1 ["A"]})))))
 
-(deftest test-60dbd000-451d-44c7-bdbb-97c73ac1f497
-  (testing "Single score with multiple letters"
+(deftest transform_test_2
+  (testing "single score with multiple letters"
     (is (= {"a" 1 "e" 1 "i" 1 "o" 1 "u" 1}
            (etl/transform {1 ["A" "E" "I" "O" "U"]})))))
 
-(deftest test-f5c5de0c-301f-4fdd-a0e5-df97d4214f54
-  (testing "Multiple scores with multiple letters"
+(deftest transform_test_3
+  (testing "multiple scores with multiple letters"
     (is (= {"a" 1 "d" 2 "e" 1  "g" 2}
            (etl/transform {1 ["A" "E"], 2 ["D" "G"]})))))
 
-(deftest test-5db8ea89-ecb4-4dcd-902f-2b418cc87b9d
-  (testing "Multiple scores with differing numbers of letters"
+(deftest transform_test_4
+  (testing "multiple scores with differing numbers of letters"
     (is (= {"a"  1 "b"  3 "c" 3 "d" 2 "e" 1
             "f"  4 "g"  2 "h" 4 "i" 1 "j" 8
             "k"  5 "l"  1 "m" 3 "n" 1 "o" 1
