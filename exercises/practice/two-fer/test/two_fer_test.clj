41b590eed1828c76586188d32c65891b9ca3a3b4
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-12 08:32:44 +0100

    Update generator.tpl
    Co-authored-by: Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>

--- exercises/practice/two-fer/test/two_fer_test.clj
+++ exercises/practice/two-fer/test/two_fer_test.clj
@@ -1,6 +1,6 @@
 (ns two-fer-test
   (:require [clojure.test :refer [deftest testing is]]
-             two-fer))
+            two-fer))
 
 (deftest two-fer_test_1
   (testing "no name given"
@@ -13,4 +13,3 @@
 (deftest two-fer_test_3
   (testing "another name given"
     (is (= "One for Bob, one for me." (two-fer/two-fer "Bob")))))
-
