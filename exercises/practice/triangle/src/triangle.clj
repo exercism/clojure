eee3090c6d15ded7cecc42df98d48f09245f261c
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-13 21:28:10 +0100

    triangle: update to latest (#746)

--- exercises/practice/triangle/src/triangle.clj
+++ exercises/practice/triangle/src/triangle.clj
@@ -1,17 +1,19 @@
 (ns triangle)
 
-(defn is-valid? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn equilateral?
+  "Returns true if the triangle with sides a, b, and c is equilateral; otherwise, returns false"
+  [a b c]
+  ;; function body
+  )
 
-(defn equilateral? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn isosceles?
+  "Returns true if the triangle with sides a, b, and c is isosceles; otherwise, returns false"
+  [a b c]
+  ;; function body
+  )
 
-(defn isosceles? [] ;; <- arglist goes here
-  ;; your code goes here
-)
-
-(defn scalene? [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn scalene?
+  "Returns true if the triangle with sides a, b, and c is scalene; otherwise, returns false"
+  [a b c]
+  ;; function body
+  )
