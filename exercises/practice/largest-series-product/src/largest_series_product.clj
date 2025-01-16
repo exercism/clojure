b7c927e61d479b2996e07d41b13187861a6ad55d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-16 17:25:53 +0200

    Update largest_series_product.clj

--- exercises/practice/largest-series-product/src/largest_series_product.clj
+++ exercises/practice/largest-series-product/src/largest_series_product.clj
@@ -1,7 +1,7 @@
 (ns largest-series-product)
 
 (defn largest-product
-  "Calculates the largest product of consecutive digits in a series of length n from the string s"
+  "Returns the largest product of consecutive digits in a series of length n from the string s"
   [n s]
   ;; function body
   )
