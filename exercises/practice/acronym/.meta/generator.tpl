19cfe7c2ad0fd8a9059d27082a2695fc2a4146a2
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-18 00:15:08 +0200

    add generator and regenerate tests

---
+++ exercises/practice/acronym/.meta/generator.tpl
@@ -0,0 +1,8 @@
+(ns acronym-test
+  (:require [clojure.test :refer [deftest testing is]]
+            acronym))
+{{#test_cases.abbreviate}}
+(deftest acronym_test_{{idx}}
+  (testing {{description}}
+    (is (= {{expected}} (acronym/acronym {{input.phrase}})))))
+{{/test_cases.abbreviate~}}
--- exercises/practice/acronym/test/acronym_test.clj
+++ exercises/practice/acronym/test/acronym_test.clj
@@ -1,24 +1,39 @@
 (ns acronym-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest testing is]]
             acronym))
 
-(deftest test-acronym-empty-string
-  (is (= "" (acronym/acronym ""))))
+(deftest acronym_test_1
+  (testing "basic"
+    (is (= "PNG" (acronym/acronym "Portable Network Graphics")))))
 
-(deftest test-acronym-png
-  (is (= "PNG" (acronym/acronym "Portable Network Graphics"))))
+(deftest acronym_test_2
+  (testing "lowercase words"
+    (is (= "ROR" (acronym/acronym "Ruby on Rails")))))
 
-(deftest test-acronym-ror
-  (is (= "ROR" (acronym/acronym "Ruby on Rails"))))
+(deftest acronym_test_3
+  (testing "punctuation"
+    (is (= "FIFO" (acronym/acronym "First In, First Out")))))
 
-(deftest test-acronym-html
-  (is (= "HTML" (acronym/acronym "HyperText Markup Language"))))
+(deftest acronym_test_4
+  (testing "all caps word"
+    (is (= "GIMP" (acronym/acronym "GNU Image Manipulation Program")))))
 
-(deftest test-acronym-fifo
-  (is (= "FIFO" (acronym/acronym "First In, First Out"))))
+(deftest acronym_test_5
+  (testing "punctuation without whitespace"
+    (is (= "CMOS" (acronym/acronym "Complementary metal-oxide semiconductor")))))
 
-(deftest test-acronym-php
-  (is (= "PHP" (acronym/acronym "PHP: Hypertext Preprocessor"))))
+(deftest acronym_test_6
+  (testing "very long abbreviation"
+    (is (= "ROTFLSHTMDCOALM" (acronym/acronym "Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me")))))
 
-(deftest test-acronym-cmos
-  (is (= "CMOS" (acronym/acronym "Complementary metal-oxide semiconductor"))))
+(deftest acronym_test_7
+  (testing "consecutive delimiters"
+    (is (= "SIMUFTA" (acronym/acronym "Something - I made up from thin air")))))
+
+(deftest acronym_test_8
+  (testing "apostrophes"
+    (is (= "HC" (acronym/acronym "Halley's Comet")))))
+
+(deftest acronym_test_9
+  (testing "underscore emphasis"
+    (is (= "TRNT" (acronym/acronym "The Road _Not_ Taken")))))
