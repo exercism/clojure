a636c73218a9880fda32695985b8ed519ac50672
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-17 13:46:24 +0200

    add generator and regenerate tests

--- exercises/practice/reverse-string/test/reverse_string_test.clj
+++ exercises/practice/reverse-string/test/reverse_string_test.clj
@@ -1,27 +1,31 @@
 (ns reverse-string-test
   (:require [clojure.test :refer [deftest testing is]]
-            [reverse-string :refer [reverse-string]]))
+            reverse-string))
 
-(deftest empty-string
-  (testing "An empty string"
-    (is (= "" (reverse-string "")))))
+(deftest reverse-string_test_1
+  (testing "an empty string"
+    (is (= "" (reverse-string/reverse-string "")))))
 
-(deftest a-word
-  (testing "A word"
-    (is (= "tobor" (reverse-string "robot")))))
+(deftest reverse-string_test_2
+  (testing "a word"
+    (is (= "tobor" (reverse-string/reverse-string "robot")))))
 
-(deftest capitalised-word
-  (testing "A capitalized word"
-    (is (= "nemaR" (reverse-string "Ramen")))))
+(deftest reverse-string_test_3
+  (testing "a capitalized word"
+    (is (= "nemaR" (reverse-string/reverse-string "Ramen")))))
 
-(deftest sentence-with-punctuation
-  (testing "A sentence with punctuation"
-    (is (= "!yrgnuh m'I" (reverse-string "I'm hungry!")))))
+(deftest reverse-string_test_4
+  (testing "a sentence with punctuation"
+    (is (= "!yrgnuh m'I" (reverse-string/reverse-string "I'm hungry!")))))
 
-(deftest palindrome
-  (testing "A palindrome"
-    (is (= "racecar" (reverse-string "racecar")))))
+(deftest reverse-string_test_5
+  (testing "a palindrome"
+    (is (= "racecar" (reverse-string/reverse-string "racecar")))))
 
-(deftest even-sized-word
-  (testing "An even sized word"
-    (is (= "reward" (reverse-string "drawer")))))
+(deftest reverse-string_test_6
+  (testing "an even-sized word"
+    (is (= "reward" (reverse-string/reverse-string "drawer")))))
+
+(deftest reverse-string_test_7
+  (testing "wide characters"
+    (is (= "猫子" (reverse-string/reverse-string "子猫")))))
