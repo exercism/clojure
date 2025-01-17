09f541a4c15d070d09304aaf7db3da7c9d3202f5
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-17 23:24:50 +0200

    update starter file

--- exercises/practice/pig-latin/src/pig_latin.clj
+++ exercises/practice/pig-latin/src/pig_latin.clj
@@ -1,5 +1,7 @@
 (ns pig-latin)
 
-(defn translate [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn translate
+  "Translates phrase from English to Pig Latin"
+  [phrase]
+  ;; function body
+  )
