14c4e2eb28df51fd9cdb510b18a00b6be65e99f2
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-18 21:18:38 +0200

    update starter file

--- exercises/practice/change/src/change.clj
+++ exercises/practice/change/src/change.clj
@@ -1,5 +1,8 @@
 (ns change)
 
-(defn issue [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn issue
+  "Given an amount to change and a set of coins, it returns the
+  fewest coins such that the sum of their values equals the change"
+  [amount coins]
+  ;; function body
+  )
