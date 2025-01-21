df6ae14a26e867d852b754c1a3a8c62d0bcc484a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-22 00:35:09 +0200

    add generators and regenerate tests

---
+++ exercises/practice/series/.meta/generator.clj
@@ -0,0 +1,6 @@
+(ns series-generator)
+
+(defn update-test-case [test-case]
+  (if-let [error (get-in test-case [:expected :error])]
+    (assoc-in test-case [:expected :error] (str "^" error "$"))
+    test-case))
--- exercises/practice/series/test/series_test.clj
+++ exercises/practice/series/test/series_test.clj
@@ -1,23 +1,58 @@
 (ns series-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [series :refer [slices]]))
+  (:require [clojure.test :refer [deftest testing is]]
+            series))
 
-(deftest empty-string
-  (testing "empty string with any number"
-    (is (= [] (slices "" 1)))))
+(deftest slices_test_1
+  (testing "slices of one from one"
+    (is (= ["1"]
+           (series/slices "1" 1)))))
 
-(deftest number-eq-zero
-  (testing "number = 0"
-    (is (= [""] (slices "123" 0)))))
+(deftest slices_test_2
+  (testing "slices of one from two"
+    (is (= ["1" "2"]
+           (series/slices "12" 1)))))
 
-(deftest number>string
-  (testing "number > string-length"
-    (is (= [] (slices "123" 1000)))))
+(deftest slices_test_3
+  (testing "slices of two"
+    (is (= ["35"]
+           (series/slices "35" 2)))))
 
-(deftest number=string
-  (testing "number = string-length"
-    (is (= ["123"] (slices "123" 3)))))
+(deftest slices_test_4
+  (testing "slices of two overlap"
+    (is (= ["91" "14" "42"]
+           (series/slices "9142" 2)))))
 
-(deftest number<string
-  (testing "number < string-length"
-    (is (= #{"123" "234" "345"} (set (slices "12345" 3))))))
+(deftest slices_test_5
+  (testing "slices can include duplicates"
+    (is (= ["777" "777" "777" "777"]
+           (series/slices "777777" 3)))))
+
+(deftest slices_test_6
+  (testing "slices of a long series"
+    (is (= ["91849" "18493" "84939" "49390" "93904" "39042" "90424" "04243"]
+           (series/slices "918493904243" 5)))))
+
+(deftest slices_test_7
+  (testing "slice length is too large"
+    (is (thrown-with-msg? IllegalArgumentException #"^slice length cannot be greater than series length$"
+                          (series/slices "12345" 6)))))
+
+(deftest slices_test_8
+  (testing "slice length is way too large"
+    (is (thrown-with-msg? IllegalArgumentException #"^slice length cannot be greater than series length$"
+                          (series/slices "12345" 42)))))
+
+(deftest slices_test_9
+  (testing "slice length cannot be zero"
+    (is (thrown-with-msg? IllegalArgumentException #"^slice length cannot be zero$"
+                          (series/slices "12345" 0)))))
+
+(deftest slices_test_10
+  (testing "slice length cannot be negative"
+    (is (thrown-with-msg? IllegalArgumentException #"^slice length cannot be negative$"
+                          (series/slices "123" -1)))))
+
+(deftest slices_test_11
+  (testing "empty series is invalid"
+    (is (thrown-with-msg? IllegalArgumentException #"^series cannot be empty$"
+                          (series/slices "" 1)))))
