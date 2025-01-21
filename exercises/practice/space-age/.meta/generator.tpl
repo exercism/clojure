50563cfa06639015c40e6ddd9ccb18d5baf2ee4a
Erik Schierboom <erik_schierboom@hotmail.com>
2025-01-21 12:14:33 +0100

    space-age: sync tests (#786)

---
+++ exercises/practice/space-age/.meta/generator.tpl
@@ -0,0 +1,14 @@
+(ns space-age-test
+  (:require [clojure.test :refer [deftest testing is]]
+            space-age))
+
+(defn- rounds-to
+  [expected actual]
+  (is (= (Math/round (* 100.0 expected))
+         (Math/round (* 100.0 actual)))))
+
+{{#test_cases.age}}
+(deftest on-{{input.planet}}_test_1
+  (testing {{description}}  
+    (rounds-to {{expected}} (space-age/on-{{input.planet}} {{input.seconds}}))))
+{{/test_cases.age}}
--- exercises/practice/space-age/src/space_age.clj
+++ exercises/practice/space-age/src/space_age.clj
@@ -1,33 +1,49 @@
 (ns space-age)
 
-(defn on-mercury [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-mercury
+  "Returns someone's age on Mercury based on the given age in seconds"
+  [seconds]
+  ;; function body
+  )
 
-(defn on-venus [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-venus
+  "Returns someone's age on Venus based on the given age in seconds"
+  [seconds]
+  ;; function body
+  )
 
-(defn on-earth [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn on-earth
+  "Returns someone's age on Earth based on the given age in seconds"
+  [seconds]
+      ;; function body
+  )
 
-(defn on-mars [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-mars
+  "Returns someone's age on Mars based on the given age in seconds"
+  [seconds]
+  ;; function body
+  )
 
-(defn on-jupiter [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-jupiter
+  "Returns someone's age on Jupiter based on the given age in seconds"
+  [seconds]
+  ;; function body
+  )
 
-(defn on-saturn [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-saturn
+  "Returns someone's age on Saturn based on the given age in seconds"
+  [seconds]
+  ;; function body
+  )
 
-(defn on-uranus [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-uranus
+  "Returns someone's age on Uranus based on the given age in seconds"
+  [seconds]
+  ;; function body
+  )
 
-(defn on-neptune [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-neptune
+  "Returns someone's age on Neptune based on the given age in seconds"
+  [seconds]
+  ;; function body
+  )
