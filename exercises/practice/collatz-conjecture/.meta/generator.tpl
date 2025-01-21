a1e9c68359acc01598f3e771f34c60853559ff3d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-21 22:49:08 +0200

    add generators and regenerate tests

---
+++ exercises/practice/collatz-conjecture/.meta/generator.tpl
@@ -0,0 +1,14 @@
+(ns collatz-conjecture-test
+  (:require [clojure.test :refer [deftest testing is]]
+            collatz-conjecture))
+
+{{#test_cases.steps}}
+(deftest collatz_test_{{idx}}
+  (testing {{description}}
+    {{~#if error}}
+    (is (thrown-with-msg? IllegalArgumentException #{{error}}
+                          (collatz-conjecture/collatz {{input.number}})))))
+    {{else}}
+    (is (= {{expected}} (collatz-conjecture/collatz {{input.number}})))))
+    {{/if~}}
+{{/test_cases.steps}}
--- exercises/practice/collatz-conjecture/test/collatz_conjecture_test.clj
+++ exercises/practice/collatz-conjecture/test/collatz_conjecture_test.clj
@@ -1,29 +1,29 @@
 (ns collatz-conjecture-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [collatz-conjecture :refer [collatz]]))
+  (:require [clojure.test :refer [deftest testing is]]
+            collatz-conjecture))
 
-(deftest steps-for-1
+(deftest collatz_test_1
   (testing "zero steps for one"
-    (is (= 0 (collatz 1)))))
+    (is (= 0 (collatz-conjecture/collatz 1)))))
 
-(deftest steps-for-16
+(deftest collatz_test_2
   (testing "divide if even"
-    (is (= 4 (collatz 16)))))
+    (is (= 4 (collatz-conjecture/collatz 16)))))
 
-(deftest steps-for-12
+(deftest collatz_test_3
   (testing "even and odd steps"
-    (is (= 9 (collatz 12)))))
+    (is (= 9 (collatz-conjecture/collatz 12)))))
 
-(deftest steps-for-1000000
-  (testing "Large number of even and odd steps"
-    (is (= 152 (collatz 1000000)))))
+(deftest collatz_test_4
+  (testing "large number of even and odd steps"
+    (is (= 152 (collatz-conjecture/collatz 1000000)))))
 
-(deftest steps-for-0
+(deftest collatz_test_5
   (testing "zero is an error"
-    (is (thrown? Throwable
-                 (collatz 0)))))
+    (is (thrown-with-msg? IllegalArgumentException #"^Only positive integers are allowed$"
+                          (collatz-conjecture/collatz 0)))))
 
-(deftest steps-for-negative
+(deftest collatz_test_6
   (testing "negative value is an error"
-    (is (thrown? Throwable
-                 (collatz -15)))))
+    (is (thrown-with-msg? IllegalArgumentException #"^Only positive integers are allowed$"
+                          (collatz-conjecture/collatz -15)))))
