85d5ebd46c76163de776da270082523342aff0bf
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-22 14:51:54 +0200

    collatz-conjecture: Add generators and regenerate tests (#797)
    * update example solution
    
    * add generators and regenerate tests
    
    * update function template
    
    * update contributors
    
    [no important files changed]

--- exercises/practice/collatz-conjecture/.meta/example.clj
+++ exercises/practice/collatz-conjecture/.meta/example.clj
@@ -1,13 +1,12 @@
 (ns collatz-conjecture)
 
 (defn collatz-helper [n]
-  (cond (= 1 n)   1
-        (even? n) (/ n 2)
-        :else     (inc (* 3 n))))
+  (cond
+    (= 1 n) 1
+    (even? n) (/ n 2)
+    :else (inc (* 3 n))))
 
 (defn collatz [n]
   (if (> n 0)
-    (count (take-while #(not= 1 %)
-                       (iterate collatz-helper n)))
-    (throw (IllegalArgumentException.
-            "Just defined for numbers greater than 0."))))
+    (count (take-while #(not= 1 %) (iterate collatz-helper n)))
+    (throw (IllegalArgumentException. "Only positive integers are allowed"))))
