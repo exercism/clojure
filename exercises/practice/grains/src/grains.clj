3943afee40d5052ac00c7bce427b8fe6385fbdfd
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-01-31 14:19:27 +0200

    grains: Sync tests (#805)
    * sync tests
    
    * add generator and regenerate tests
    
    * update example solution
    
    * update function template
    
    * update contributors
    
    [no important files changed]

--- exercises/practice/grains/src/grains.clj
+++ exercises/practice/grains/src/grains.clj
@@ -1,9 +1,13 @@
 (ns grains)
 
-(defn square [] ;; <- arglist goes here
-    ;; your code goes here
-)
+(defn square
+  "Returns the number of grains on the n-th chessboard square."
+  [n]
+  ;; function body
+  )
 
-(defn total []  ;; <- arglist goes here
-    ;; your code goes here
-)
+(defn total
+  "Returns the total number of grains on the chessboard."
+  []
+  ;; function body
+  )
