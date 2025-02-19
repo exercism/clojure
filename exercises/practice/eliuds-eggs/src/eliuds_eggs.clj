a82406975b242eb34eb8bf318dbf8fad530bd14b
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-02-19 18:21:50 +0200

    update function template

--- exercises/practice/eliuds-eggs/src/eliuds_eggs.clj
+++ exercises/practice/eliuds-eggs/src/eliuds_eggs.clj
@@ -1,5 +1,7 @@
 (ns eliuds-eggs)
 
-(defn egg-count [number]
-    ;; your code goes here
-)
+(defn egg-count
+  "Returns the number of 1 bits in the binary representation of the given number."
+  [number]
+  ;; function body
+  )
