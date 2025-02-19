fcf3c22268a6c383d54ea1106c68309880c3d95f
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-02-19 22:05:29 +0200

    eliuds-eggs: Add generators and regenerate tests (#811)
    * add generators and regenerate tests
    
    * update function template

--- exercises/practice/eliuds-eggs/test/eliuds_eggs_test.clj
+++ exercises/practice/eliuds-eggs/test/eliuds_eggs_test.clj
@@ -1,15 +1,19 @@
 (ns eliuds-eggs-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest testing is]]
             eliuds-eggs))
 
-(deftest no-eggs
-  (is (= 0 (eliuds-eggs/egg-count 0))))
+(deftest egg-count_test_1
+  (testing "0 eggs"
+    (is (= 0 (eliuds-eggs/egg-count 0)))))
 
-(deftest one-egg
-  (is (= 1 (eliuds-eggs/egg-count 16))))
+(deftest egg-count_test_2
+  (testing "1 egg"
+    (is (= 1 (eliuds-eggs/egg-count 16)))))
 
-(deftest four-eggs
-  (is (= 4 (eliuds-eggs/egg-count 89))))
+(deftest egg-count_test_3
+  (testing "4 eggs"
+    (is (= 4 (eliuds-eggs/egg-count 89)))))
 
-(deftest thirteen-eggs
-  (is (= 13 (eliuds-eggs/egg-count 2000000000))))
+(deftest egg-count_test_4
+  (testing "13 eggs"
+    (is (= 13 (eliuds-eggs/egg-count 2000000000)))))
