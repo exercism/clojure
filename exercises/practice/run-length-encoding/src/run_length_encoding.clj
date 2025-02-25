353aa03138026614eae9b7dc07ebc7ff889b5cf0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-02-25 04:05:42 +0200

    update function templates in starter file

--- exercises/practice/run-length-encoding/src/run_length_encoding.clj
+++ exercises/practice/run-length-encoding/src/run_length_encoding.clj
@@ -1,9 +1,13 @@
 (ns run-length-encoding)
 
 (defn run-length-encode
-  "encodes a string with run-length-encoding"
-  [plain-text])
+  "Encodes a string with run-length encoding."
+  [plain-text]
+  ;; function body
+  )
 
 (defn run-length-decode
-  "decodes a run-length-encoded string"
-  [cipher-text])
+  "Decodes a run-length-encoded string."
+  [cipher-text]
+  ;; function body
+  )
