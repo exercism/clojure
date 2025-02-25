5815fbb7fafb2d7a7242973a69d2199e0ca853cb
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-02-25 23:01:39 +0200

    prime-factors: Update to latest (#825)
    * sync tests
    
    * add generator and generate tests
    
    * update function template in starter file
    
    * simplify example solution
    
    * update contributors

--- exercises/practice/prime-factors/.meta/example.clj
+++ exercises/practice/prime-factors/.meta/example.clj
@@ -1,14 +1,12 @@
 (ns prime-factors)
 
-(defn- least-prime-divisor
-  [number]
-  (or (first
-       (filter #(zero? (rem number %1))
-               (range 2 (inc (/ number 2)))))
-      number))
-
-(defn of [number]
-  (if
-   (< number 2) []
-   (let [divisor (least-prime-divisor number)]
-     (into [divisor] (of (/ number divisor))))))
+(defn of
+  [n]
+  (loop [result []
+         candidate 2
+         n n]
+    (if (= n 1)
+      result
+      (if (zero? (mod n candidate))
+        (recur (conj result candidate) candidate (quot n candidate))
+        (recur result (inc candidate) n)))))
--- exercises/practice/prime-factors/test/prime_factors_test.clj
+++ exercises/practice/prime-factors/test/prime_factors_test.clj
@@ -1,36 +1,63 @@
 (ns prime-factors-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest testing is]]
             prime-factors))
 
-(deftest one
-  (is (= [] (prime-factors/of 1))))
+(deftest of_test_1
+  (testing "no factors"
+    (is (= []
+           (prime-factors/of 1)))))
 
-(deftest two
-  (is (= [2] (prime-factors/of 2))))
+(deftest of_test_2
+  (testing "prime number"
+    (is (= [2]
+           (prime-factors/of 2)))))
 
-(deftest three
-  (is (= [3] (prime-factors/of 3))))
+(deftest of_test_3
+  (testing "another prime number"
+    (is (= [3]
+           (prime-factors/of 3)))))
 
-(deftest four
-  (is (= [2, 2] (prime-factors/of 4))))
+(deftest of_test_4
+  (testing "square of a prime"
+    (is (= [3 3]
+           (prime-factors/of 9)))))
 
-(deftest six
-  (is (= [2, 3] (prime-factors/of 6))))
+(deftest of_test_5
+  (testing "product of first prime"
+    (is (= [2 2]
+           (prime-factors/of 4)))))
 
-(deftest eight
-  (is (= [2, 2, 2] (prime-factors/of 8))))
+(deftest of_test_6
+  (testing "cube of a prime"
+    (is (= [2 2 2]
+           (prime-factors/of 8)))))
 
-(deftest nine
-  (is (= [3, 3] (prime-factors/of 9))))
+(deftest of_test_7
+  (testing "product of second prime"
+    (is (= [3 3 3]
+           (prime-factors/of 27)))))
 
-(deftest twenty-seven
-  (is (= [3, 3, 3] (prime-factors/of 27))))
+(deftest of_test_8
+  (testing "product of third prime"
+    (is (= [5 5 5 5]
+           (prime-factors/of 625)))))
 
-(deftest six-hundred-twenty-five
-  (is (= [5, 5, 5, 5] (prime-factors/of 625))))
+(deftest of_test_9
+  (testing "product of first and second prime"
+    (is (= [2 3]
+           (prime-factors/of 6)))))
 
-(deftest a-large-number
-  (is (= [5, 17, 23, 461] (prime-factors/of 901255))))
+(deftest of_test_10
+  (testing "product of primes and non-primes"
+    (is (= [2 2 3]
+           (prime-factors/of 12)))))
 
-(deftest a-huge-number
-  (is (= [11, 9539, 894119] (prime-factors/of 93819012551))))
+(deftest of_test_11
+  (testing "product of primes"
+    (is (= [5 17 23 461]
+           (prime-factors/of 901255)))))
+
+(deftest of_test_12
+  (testing "factors include a large prime"
+    (is (= [11 9539 894119]
+           (prime-factors/of 93819012551)))))
