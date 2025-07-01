897d62f4563e90d46e767ef9de31f8e38be764f7
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-06-30 20:29:58 -0700

    Add `flower-field`

---
+++ exercises/practice/flower-field/project.clj
@@ -0,0 +1,4 @@
+(defproject flower-field "0.1.0-SNAPSHOT"
+  :description "flower-field exercise."
+  :url "https://github.com/exercism/clojure/tree/main/exercises/practice/flower-field"
+  :dependencies [[org.clojure/clojure "1.12.0"]])
