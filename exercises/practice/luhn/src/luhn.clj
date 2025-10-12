0fb9ba6d3cd5ded7a0904f9a7120390684b422fc
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-10-12 14:13:24 +0300

    luhn

--- exercises/practice/luhn/src/luhn.clj
+++ exercises/practice/luhn/src/luhn.clj
@@ -1,8 +1,7 @@
 (ns luhn)
 
 (defn valid?
-  "Returns true if the given number is valid according
-  to the luhn algorithm; otherwise, returns false."
+  "Returns true if the given number is valid; otherwise, it returns false."
   [s]
   ;; function body
   )
