438679d02423d829b1506b865643e76a8bcc04d3
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-10-12 14:45:36 +0300

    two-fer

--- exercises/practice/two-fer/src/two_fer.clj
+++ exercises/practice/two-fer/src/two_fer.clj
@@ -1,7 +1,7 @@
 (ns two-fer)
 
 (defn two-fer
-  "Return what you will say as you give away the extra cookie"
+  "Returns what you will say as you give away the extra cookie."
   [name]
   ;; function body
   )
