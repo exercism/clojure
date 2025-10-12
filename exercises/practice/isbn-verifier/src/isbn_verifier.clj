2f7a2dd0d0a049280d4fd712412599d52d9d001d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-10-12 14:09:15 +0300

    isbn

--- exercises/practice/isbn-verifier/src/isbn_verifier.clj
+++ exercises/practice/isbn-verifier/src/isbn_verifier.clj
@@ -1,7 +1,7 @@
 (ns isbn-verifier)
 
 (defn isbn?
-  "Returns true if the given isbn is valid; otherwise, returns false"
+  "Returns true if the given isbn is valid; otherwise, it returns false."
   [isbn]
   ;; function body
   )
