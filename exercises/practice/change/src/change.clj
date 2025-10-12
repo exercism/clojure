9c4099f385acc866896d8eec3531ef304ff127cc
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-10-12 13:03:50 +0300

    change

--- exercises/practice/change/src/change.clj
+++ exercises/practice/change/src/change.clj
@@ -1,8 +1,7 @@
 (ns change)
 
 (defn issue
-  "Given an amount to change and a set of coins, it returns the
-  fewest coins such that the sum of their values equals the change"
+  "Given an amount to change and a set of coins, it returns the fewest coins such that the sum of their values equals the change."
   [amount coins]
   ;; function body
   )
