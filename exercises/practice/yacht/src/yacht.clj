9fef07919f6628f552e973c17594847f20578fb9
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-10-12 14:48:54 +0300

    yacht

--- exercises/practice/yacht/src/yacht.clj
+++ exercises/practice/yacht/src/yacht.clj
@@ -1,7 +1,7 @@
 (ns yacht)
 
 (defn score
-  "Given five dice and a category, it calculates the score of the dice for that category"
+  "Given five dice and a category, it calculates the score of the dice for that category."
   [dice category]
   ;; function body
   )
