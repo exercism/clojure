f187e03a822080d5db911e3dc0d5623187bb1c83
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-10-12 23:09:31 +0300

    Move list-ops appendix solution to main example.clj (#857)

--- exercises/practice/list-ops/.meta/example.clj
+++ exercises/practice/list-ops/.meta/example.clj
@@ -48,3 +48,53 @@
     (if (neg? index)
       result
       (recur (dec index) (conj result (coll index))))))
+
+
+;;Solves the appendix challenge
+;;
+;;(ns list-ops)
+;;
+;;(declare foldl)
+;;(declare reverse-order)
+;;
+;;(defn append
+;;  [coll1 coll2]
+;;  (reverse-order (foldl conj coll2 (reverse-order coll1))))
+;;
+;;(defn concatenate
+;;  [colls]
+;;  (foldl append colls ()))
+;;
+;;(defn select-if
+;;  [pred coll]
+;;  (let [reducer (fn [acc el]
+;;                  (if (pred el)
+;;                    (conj acc el)
+;;                    acc))]
+;;    (reverse-order (foldl reducer coll ()))))
+;;
+;;(defn length
+;;  [coll]
+;;  (let [reducer (fn [acc _]
+;;                  (inc acc))]
+;;    (foldl reducer coll 0)))
+;;
+;;(defn apply-to-each
+;;  [f coll]
+;;  (let [reducer (fn [acc el]
+;;                  (conj acc (f el)))]
+;;    (reverse-order (foldl reducer coll ()))))
+;;
+;;(defn foldl
+;;  [f coll acc]
+;;  (if (seq coll)
+;;    (recur f (rest coll) (f acc (first coll)))
+;;    acc))
+;;
+;;(defn foldr
+;;  [f coll acc]
+;;  (foldl f (reverse-order coll) acc))
+;;
+;;(defn reverse-order
+;;  [coll]
+;;  (foldl conj coll ()))
