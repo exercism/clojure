8f7540cf5fc5d48aa23b9823f49a13e3a9754d7a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-10-12 14:09:51 +0300

    isogram

--- exercises/practice/isogram/src/isogram.clj
+++ exercises/practice/isogram/src/isogram.clj
@@ -1,7 +1,7 @@
 (ns isogram)
 
 (defn isogram?
-  "Returns true if the given string is an isogram; otherwise, returns false"
+  "Returns true if the given string is an isogram; otherwise, it returns false."
   [s]
   ;; function body
   )
