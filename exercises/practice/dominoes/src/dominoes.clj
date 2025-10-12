c49a7d79c0585414ffb320f720cff7b2f1cb6aac
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-10-12 13:10:05 +0300

    dominoes

--- exercises/practice/dominoes/src/dominoes.clj
+++ exercises/practice/dominoes/src/dominoes.clj
@@ -1,7 +1,7 @@
 (ns dominoes)
 
 (defn can-chain?
-  "Given a collection of dominoes, it returns true if they can be arranged into a chain; otherwise, it returns false"
+  "Given a collection of dominoes, it returns true if they can be arranged into a chain; otherwise, it returns false."
   [dominoes]
   ;; function body
   )
