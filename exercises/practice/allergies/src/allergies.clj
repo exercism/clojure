67d011df8cca57915f0ffcad599788d32e1a447e
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-10-12 12:53:05 +0300

    allergies

--- exercises/practice/allergies/src/allergies.clj
+++ exercises/practice/allergies/src/allergies.clj
@@ -1,13 +1,13 @@
 (ns allergies)
 
 (defn allergic-to?
-  "Returns true if the score indicates an allergy to the allergen; otherwise, returns false"
+  "Returns true if the score indicates an allergy to the allergen; otherwise, it returns false."
   [score allergen]
   ;; function body
   )
 
 (defn allergies
-  "Returns all allergens associated with the score"
+  "Returns all allergens associated with the score."
   [score]
   ;; function body
   )
