59e7305b9730a19e652aa4c4a13d62a34e6628ff
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2025-10-12 14:24:07 +0300

    space-age

--- exercises/practice/space-age/src/space_age.clj
+++ exercises/practice/space-age/src/space_age.clj
@@ -1,49 +1,49 @@
 (ns space-age)
 
 (defn on-mercury
-  "Returns someone's age on Mercury based on the given age in seconds"
+  "Returns someone's age on Mercury based on the given age in seconds."
   [seconds]
   ;; function body
   )
 
 (defn on-venus
-  "Returns someone's age on Venus based on the given age in seconds"
+  "Returns someone's age on Venus based on the given age in seconds."
   [seconds]
   ;; function body
   )
 
 (defn on-earth
-  "Returns someone's age on Earth based on the given age in seconds"
+  "Returns someone's age on Earth based on the given age in seconds."
   [seconds]
       ;; function body
   )
 
 (defn on-mars
-  "Returns someone's age on Mars based on the given age in seconds"
+  "Returns someone's age on Mars based on the given age in seconds."
   [seconds]
   ;; function body
   )
 
 (defn on-jupiter
-  "Returns someone's age on Jupiter based on the given age in seconds"
+  "Returns someone's age on Jupiter based on the given age in seconds."
   [seconds]
   ;; function body
   )
 
 (defn on-saturn
-  "Returns someone's age on Saturn based on the given age in seconds"
+  "Returns someone's age on Saturn based on the given age in seconds."
   [seconds]
   ;; function body
   )
 
 (defn on-uranus
-  "Returns someone's age on Uranus based on the given age in seconds"
+  "Returns someone's age on Uranus based on the given age in seconds."
   [seconds]
   ;; function body
   )
 
 (defn on-neptune
-  "Returns someone's age on Neptune based on the given age in seconds"
+  "Returns someone's age on Neptune based on the given age in seconds."
   [seconds]
   ;; function body
   )
