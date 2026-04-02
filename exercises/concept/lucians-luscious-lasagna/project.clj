bad3fcabe0eb77da205fabf4a0e5f42d38ceb53a
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2026-04-03 01:46:39 +0300

    lucians-luscious-lasagna: Add the ability to select which tests to run

--- exercises/concept/lucians-luscious-lasagna/project.clj
+++ exercises/concept/lucians-luscious-lasagna/project.clj
@@ -1,4 +1,12 @@
 (defproject lucians-luscious-lasagna "0.1.0-SNAPSHOT"
   :description "lucians-luscious-lasagna exercise."
   :url "https://github.com/exercism/clojure/tree/main/exercises/concept/lucians-luscious-lasagna"
-  :dependencies [[org.clojure/clojure "1.12.0"]])
+  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :test-selectors {:task-1 :task-1
+                   :task-2 :task-2
+                   :task-3 :task-3
+                   :task-4 :task-4
+                   :expected-time :expected-time
+                   :remaining-time :remaining-time
+                   :prep-time :prep-time
+                   :total-time :total-time})
--- exercises/concept/lucians-luscious-lasagna/test/lucians_luscious_lasagna_test.clj
+++ exercises/concept/lucians-luscious-lasagna/test/lucians_luscious_lasagna_test.clj
@@ -2,20 +2,20 @@
   (:require [clojure.test :refer [deftest is]]
             lucians-luscious-lasagna))
 
-(deftest ^{:task 1} expected-time-test
+(deftest ^{:task 1 :task-1 true :expected-time true} expected-time-test
   (is (= 40 lucians-luscious-lasagna/expected-time)))
 
-(deftest ^{:task 2} remaining-time-test
+(deftest ^{:task 2 :task-2 true :remaining-time true} remaining-time-test
   (is (= 15 (lucians-luscious-lasagna/remaining-time 25))))
 
-(deftest ^{:task 3} prep-time-one-layer-test
+(deftest ^{:task 3 :task-3 true :prep-time true} prep-time-one-layer-test
   (is (= 2 (lucians-luscious-lasagna/prep-time 1))))
 
-(deftest ^{:task 3} prep-time-multiple-layers-test
+(deftest ^{:task 3 :task-3 true :prep-time true} prep-time-multiple-layers-test
   (is (= 8 (lucians-luscious-lasagna/prep-time 4))))
 
-(deftest ^{:task 4} total-time-one-layer-test
+(deftest ^{:task 4 :task-4 true :total-time true} total-time-one-layer-test
   (is (= 32 (lucians-luscious-lasagna/total-time 1 30))))
 
-(deftest ^{:task 4} total-time-multiple-layers-test
+(deftest ^{:task 4 :task-4 true :total-time true} total-time-multiple-layers-test
   (is (= 16 (lucians-luscious-lasagna/total-time 4 8))))
