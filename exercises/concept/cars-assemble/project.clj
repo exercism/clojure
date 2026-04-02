f69d93f661c7085cf8af45f3a622117daa85e780
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2026-04-02 23:57:47 +0300

    cars-assemble: Add the ability to select which tests to run

--- exercises/concept/cars-assemble/project.clj
+++ exercises/concept/cars-assemble/project.clj
@@ -1,4 +1,8 @@
 (defproject cars-assemble "0.1.0-SNAPSHOT"
   :description "cars-assemble exercise."
   :url "https://github.com/exercism/clojure/tree/main/exercises/concept/cars-assemble"
-  :dependencies [[org.clojure/clojure "1.12.0"]])
+  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :test-selectors {:task-1 :task-1
+                   :task-2 :task-2
+                   :production-rate :production-rate
+                   :working-items :working-items})
