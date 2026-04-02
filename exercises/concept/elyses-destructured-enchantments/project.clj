4d72f1cb5343157bc33b46fa19095208b32b21ff
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2026-04-03 00:18:38 +0300

    elyses-destructured-enchantments: Add the ability to select which tests to run

--- exercises/concept/elyses-destructured-enchantments/project.clj
+++ exercises/concept/elyses-destructured-enchantments/project.clj
@@ -1,4 +1,14 @@
 (defproject elyses-destructured-enchantments "0.1.0-SNAPSHOT"
   :description "elyses-destructured-enchantments exercise."
   :url "https://github.com/exercism/clojure/tree/main/exercises/concept/elyses-destructured-enchantments"
-  :dependencies [[org.clojure/clojure "1.12.0"]])
+  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :test-selectors {:task-1 :task-1
+                   :task-2 :task-2
+                   :task-3 :task-3
+                   :task-4 :task-4
+                   :task-5 :task-5
+                   :first-card :first-card
+                   :second-card :second-card
+                   :swap-top-two-cards :swap-top-two-cards
+                   :discard-top-card :discard-top-card
+                   :insert-face-cards :insert-face-cards})
