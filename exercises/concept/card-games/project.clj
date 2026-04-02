17ffccaa4b93f706ae1b03ba1b53259ae3d317c1
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2026-04-02 23:26:16 +0300

    card-games: Add the ability to select which tests to run

--- exercises/concept/card-games/project.clj
+++ exercises/concept/card-games/project.clj
@@ -1,4 +1,18 @@
 (defproject bird-watcher "0.1.0-SNAPSHOT"
   :description "card-games exercise."
   :url "https://github.com/exercism/clojure/tree/main/exercises/concept/card-games"
-  :dependencies [[org.clojure/clojure "1.12.0"]])
+  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :test-selectors {:task-1 :task-1
+                   :task-2 :task-2
+                   :task-3 :task-3
+                   :task-4 :task-4
+                   :task-5 :task-5
+                   :task-6 :task-6
+                   :task-7 :task-7
+                   :rounds :rounds
+                   :concat-rounds :concat-rounds
+                   :contains-round? :contains-round?
+                   :card-average :card-average
+                   :approx-average? :approx-average?
+                   :average-even-odd? :average-even-odd?
+                   :maybe-double-last :maybe-double-last})
