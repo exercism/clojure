45d2c9f703d6d7c90102cde1df7948d5272967b0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2026-04-02 21:40:29 +0300

    annalyns-infiltration: Add the ability to select which tests to run

--- exercises/concept/annalyns-infiltration/project.clj
+++ exercises/concept/annalyns-infiltration/project.clj
@@ -1,4 +1,12 @@
 (defproject annalyns-infiltration "0.1.0-SNAPSHOT"
   :description "annalyns-infiltration exercise."
   :url "https://github.com/exercism/clojure/tree/main/exercises/concept/annalyns-infiltration"
-  :dependencies [[org.clojure/clojure "1.12.0"]])
+  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :test-selectors {:task-1 :task-1
+                   :task-2 :task-2
+                   :task-3 :task-3
+                   :task-4 :task-4
+                   :can-fast-attack? :can-fast-attack?
+                   :can-spy? :can-spy?
+                   :can-signal-prisoner? :can-signal-prisoner?
+                   :can-free-prisoner? :can-free-prisoner?})
