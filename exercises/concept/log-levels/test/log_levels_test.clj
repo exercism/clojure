0603fdeea4b279e3592c67bed1c59c2a3fa9e949
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2026-04-03 01:39:05 +0300

    log-levels: Add the ability to select which tests to run

--- exercises/concept/log-levels/test/log_levels_test.clj
+++ exercises/concept/log-levels/test/log_levels_test.clj
@@ -2,35 +2,35 @@
   (:require [clojure.test :refer [deftest is]]
             log-levels))
 
-(deftest ^{:task 1} message-error-test
+(deftest ^{:task 1 :task-1 true :message true} message-error-test
   (is (= "Stack overflow" (log-levels/message "[ERROR]: Stack overflow"))))
 
-(deftest ^{:task 1} message-warning-test
+(deftest ^{:task 1 :task-1 true :message true} message-warning-test
   (is (= (log-levels/message "[WARNING]: Disk almost full") "Disk almost full")))
 
-(deftest ^{:task 1} message-info-test
+(deftest ^{:task 1 :task-1 true :message true} message-info-test
   (is (= (log-levels/message "[INFO]: File moved") "File moved")))
 
-(deftest ^{:task 1} message-trim-whitespace-test
+(deftest ^{:task 1 :task-1 true :message true} message-trim-whitespace-test
   (is (= "Timezone not set" (log-levels/message "[WARNING]:   \tTimezone not set  \r\n"))))
 
-(deftest ^{:task 2} log-level-error-test
+(deftest ^{:task 2 :task-2 true :log-level true} log-level-error-test
   (is (= "error" (log-levels/log-level "[ERROR]: Disk full"))))
 
-(deftest ^{:task 2} log-level-warning-test
+(deftest ^{:task 2 :task-2 true :log-level true} log-level-warning-test
   (is (= "warning" (log-levels/log-level "[WARNING]: Unsafe password"))))
 
-(deftest ^{:task 2} log-level-info-test
+(deftest ^{:task 2 :task-2 true :log-level true} log-level-info-test
   (is (= "info" (log-levels/log-level "[INFO]: Timezone changed"))))
 
-(deftest ^{:task 3} reformat-error-test
+(deftest ^{:task 3 :task-3 true :reformat true} reformat-error-test
   (is (= "Segmentation fault (error)" (log-levels/reformat "[ERROR]: Segmentation fault"))))
 
-(deftest ^{:task 3} reformat-warning-test
+(deftest ^{:task 3 :task-3 true :reformat true} reformat-warning-test
   (is (= "Decreased performance (warning)" (log-levels/reformat "[WARNING]: Decreased performance"))))
 
-(deftest ^{:task 3} reformat-info-test
+(deftest ^{:task 3 :task-3 true :reformat true} reformat-info-test
   (is (= "Disk defragmented (info)" (log-levels/reformat "[INFO]: Disk defragmented"))))
 
-(deftest ^{:task 3} reformat-trim-whitespace-test
+(deftest ^{:task 3 :task-3 true :reformat true} reformat-trim-whitespace-test
   (is (= "Corrupt disk (error)" (log-levels/reformat "[ERROR]: \t Corrupt disk\t \t \r\n"))))
