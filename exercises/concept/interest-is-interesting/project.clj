7e5790bfa1c609a57455541fc99887327a4fbd56
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2026-04-03 13:21:54 +0300

    interest-is-interesting: Add the ability to select which tests to run (#869)

--- exercises/concept/interest-is-interesting/project.clj
+++ exercises/concept/interest-is-interesting/project.clj
@@ -1,4 +1,10 @@
 (defproject interest-is-interesting "0.1.0-SNAPSHOT"
   :description "interest-is-interesting exercise."
   :url "https://github.com/exercism/clojure/tree/main/exercises/concept/interest-is-interesting"
-  :dependencies [[org.clojure/clojure "1.12.0"]])
+  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :test-selectors {:task-1 :task-1
+                   :task-2 :task-2
+                   :task-3 :task-3
+                   :interest-rate :interest-rate
+                   :annual-balance-update :annual-balance-update
+                   :amount-to-donate :amount-to-donate})
