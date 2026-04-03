e1e9c19c8e453f555306763dad54e3a1791b1980
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2026-04-03 13:21:35 +0300

    log-levels: Add the ability to select which tests to run (#868)

--- exercises/concept/log-levels/project.clj
+++ exercises/concept/log-levels/project.clj
@@ -1,4 +1,10 @@
 (defproject log-levels "0.1.0-SNAPSHOT"
   :description "log-levels exercise."
   :url "https://github.com/exercism/clojure/tree/main/exercises/concept/log-levels"
-  :dependencies [[org.clojure/clojure "1.12.0"]])
+  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :test-selectors {:task-1 :task-1
+                   :task-2 :task-2
+                   :task-3 :task-3
+                   :message :message
+                   :log-level :log-level
+                   :reformat :reformat})
