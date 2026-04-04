0a0782b7f683c06f96fba987ff62e30e60cc7571
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2026-04-04 22:30:36 +0300

    grains: Add the ability to select which tests to run (#878)
    [no important files changed]

--- exercises/practice/grains/project.clj
+++ exercises/practice/grains/project.clj
@@ -1,4 +1,6 @@
 (defproject grains "0.1.0-SNAPSHOT"
   :description "grains exercise."
   :url "https://github.com/exercism/clojure/tree/main/exercises/practice/grains"
-  :dependencies [[org.clojure/clojure "1.12.0"]])
+  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :test-selectors {:square :square
+                   :total :total})
--- exercises/practice/grains/test/grains_test.clj
+++ exercises/practice/grains/test/grains_test.clj
@@ -2,34 +2,34 @@
   (:require [clojure.test :refer [deftest testing is]]
             grains))
 
-(deftest square_test_1
+(deftest ^:square square_test_1
   (testing "grains on square 1"
     (is (= 1 (grains/square 1)))))
 
-(deftest square_test_2
+(deftest ^:square square_test_2
   (testing "grains on square 2"
     (is (= 2 (grains/square 2)))))
 
-(deftest square_test_3
+(deftest ^:square square_test_3
   (testing "grains on square 3"
     (is (= 4 (grains/square 3)))))
 
-(deftest square_test_4
+(deftest ^:square square_test_4
   (testing "grains on square 4"
     (is (= 8 (grains/square 4)))))
 
-(deftest square_test_5
+(deftest ^:square square_test_5
   (testing "grains on square 16"
     (is (= 32768 (grains/square 16)))))
 
-(deftest square_test_6
+(deftest ^:square square_test_6
   (testing "grains on square 32"
     (is (= 2147483648 (grains/square 32)))))
 
-(deftest square_test_7
+(deftest ^:square square_test_7
   (testing "grains on square 64"
     (is (= 9223372036854775808 (grains/square 64)))))
 
-(deftest total_test_1
+(deftest ^:total total_test_1
   (testing "returns the total number of grains on the board"
     (is (= 18446744073709551615 (grains/total)))))
