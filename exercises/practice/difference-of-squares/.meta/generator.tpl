765f82400499abc6695facb9df801f534a05d16b
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2026-04-04 22:27:13 +0300

    difference-of-squares: Add the ability to select which tests to run (#890)
    [no important files changed]

--- exercises/practice/difference-of-squares/.meta/generator.tpl
+++ exercises/practice/difference-of-squares/.meta/generator.tpl
@@ -3,19 +3,19 @@
             difference-of-squares))
 
 {{#test_cases.squareOfSum}}
-(deftest square-of-sum_test_{{idx}}
+(deftest ^:square-of-sum square-of-sum_test_{{idx}}
   (testing {{context}}
     (is (= {{expected}} (difference-of-squares/square-of-sum {{input.number}})))))
 {{/test_cases.squareOfSum}}
 
 {{#test_cases.sumOfSquares}}
-(deftest sum-of-squares_test_{{idx}}
+(deftest ^:sum-of-squares sum-of-squares_test_{{idx}}
   (testing {{context}}
     (is (= {{expected}} (difference-of-squares/sum-of-squares {{input.number}})))))
 {{/test_cases.sumOfSquares}}
 
 {{#test_cases.differenceOfSquares}}
-(deftest difference_test_{{idx}}
+(deftest ^:difference difference_test_{{idx}}
   (testing {{context}}
     (is (= {{expected}} (difference-of-squares/difference {{input.number}})))))
 {{/test_cases.differenceOfSquares}}
--- exercises/practice/difference-of-squares/test/difference_of_squares_test.clj
+++ exercises/practice/difference-of-squares/test/difference_of_squares_test.clj
@@ -2,38 +2,38 @@
   (:require [clojure.test :refer [deftest testing is]]
             difference-of-squares))
 
-(deftest square-of-sum_test_1
+(deftest ^:square-of-sum square-of-sum_test_1
   (testing "square of sum 1"
     (is (= 1 (difference-of-squares/square-of-sum 1)))))
 
-(deftest square-of-sum_test_2
+(deftest ^:square-of-sum square-of-sum_test_2
   (testing "square of sum 5"
     (is (= 225 (difference-of-squares/square-of-sum 5)))))
 
-(deftest square-of-sum_test_3
+(deftest ^:square-of-sum square-of-sum_test_3
   (testing "square of sum 100"
     (is (= 25502500 (difference-of-squares/square-of-sum 100)))))
 
-(deftest sum-of-squares_test_1
+(deftest ^:sum-of-squares sum-of-squares_test_1
   (testing "sum of squares 1"
     (is (= 1 (difference-of-squares/sum-of-squares 1)))))
 
-(deftest sum-of-squares_test_2
+(deftest ^:sum-of-squares sum-of-squares_test_2
   (testing "sum of squares 5"
     (is (= 55 (difference-of-squares/sum-of-squares 5)))))
 
-(deftest sum-of-squares_test_3
+(deftest ^:sum-of-squares sum-of-squares_test_3
   (testing "sum of squares 100"
     (is (= 338350 (difference-of-squares/sum-of-squares 100)))))
 
-(deftest difference_test_1
+(deftest ^:difference difference_test_1
   (testing "difference of squares 1"
     (is (= 0 (difference-of-squares/difference 1)))))
 
-(deftest difference_test_2
+(deftest ^:difference difference_test_2
   (testing "difference of squares 5"
     (is (= 170 (difference-of-squares/difference 5)))))
 
-(deftest difference_test_3
+(deftest ^:difference difference_test_3
   (testing "difference of squares 100"
     (is (= 25164150 (difference-of-squares/difference 100)))))
