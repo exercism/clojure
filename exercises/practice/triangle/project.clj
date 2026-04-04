80b2915e6f4d8eebdc50a89d53da34fe256f565d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2026-04-04 06:05:13 +0300

    triangle: Add the ability to select which tests to run

--- exercises/practice/triangle/project.clj
+++ exercises/practice/triangle/project.clj
@@ -1,4 +1,7 @@
 (defproject triangle "0.1.0-SNAPSHOT"
   :description "triangle exercise."
   :url "https://github.com/exercism/clojure/tree/main/exercises/practice/triangle"
-  :dependencies [[org.clojure/clojure "1.12.0"]])
+  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :test-selectors {:equilateral? :equilateral?
+                   :isosceles? :isosceles?
+                   :scalene? :scalene?})
