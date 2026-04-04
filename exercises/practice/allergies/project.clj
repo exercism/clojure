3901abafcbbe257714e3afaa7f7ae4c6d8a6dd30
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2026-04-04 05:09:01 +0300

    allergies: Add the ability to select which tests to run

--- exercises/practice/allergies/project.clj
+++ exercises/practice/allergies/project.clj
@@ -1,4 +1,6 @@
 (defproject allergies "0.1.0-SNAPSHOT"
   :description "allergies exercise."
   :url "https://github.com/exercism/clojure/tree/main/exercises/practice/allergies"
-  :dependencies [[org.clojure/clojure "1.12.0"]])
+  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :test-selectors {:allergic-to? :allergic-to?
+                   :allergies :allergies})
