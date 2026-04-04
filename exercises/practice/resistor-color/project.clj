1f255e910eca25215cee0ca9bb1589e4b8c06846
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2026-04-04 05:56:26 +0300

    resistor-color: Add the ability to select which tests to run

--- exercises/practice/resistor-color/project.clj
+++ exercises/practice/resistor-color/project.clj
@@ -1,4 +1,6 @@
 (defproject resistor-color "0.1.0-SNAPSHOT"
   :description "resistor-color exercise."
   :url "https://github.com/exercism/clojure/tree/main/exercises/practice/resistor-color"
-  :dependencies [[org.clojure/clojure "1.12.0"]])
+  :dependencies [[org.clojure/clojure "1.12.0"]]
+  :test-selectors {:colors :colors
+                   :color-code :color-code})
